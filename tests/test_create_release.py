54a3116605e7e3fbac2b923e7d39e715abbbe308
Mike Long <mike@compliancedb.com>
2020-09-12 15:20:06 +0200

    Implement commit list between two commits

--- tests/test_create_release.py
+++ tests/test_create_release.py
@@ -2,11 +2,11 @@
 """
 We have a test repo with a commit graph like this:
 
-    * e0d1acf (HEAD -> release-branch) Initial release commit
-    * 8f5b384 (master) Fourth commit
-    * e0ad84e Third commit
-    * b6c9e60 (production) Second commit
-    * b7e6aa6 Initial commit
+    * e0d1acf1adb9e263c1b6e0cfe3e0d2c1ade371e1 2020-09-12 (HEAD -> release-branch)  Initial release commit (Mike Long)
+    * 8f5b384644eb83e7f2a6d9499539a077e7256b8b 2020-09-12 (master)  Fourth commit (Mike Long)
+    * e0ad84e1a2464a9486e777c1ecde162edff930a9 2020-09-12  Third commit (Mike Long)
+    * b6c9e60f281e37d912ec24f038b7937f79723fb4 2020-09-12 (production)  Second commit (Mike Long)
+    * b7e6aa63087fcb1e64a5f2a99c8d255415d8cb99 2020-09-12  Initial commit (Mike Long)
 
 Get the artifact SHA from CDB using latest policy
 Get the list of commits
@@ -16,7 +16,30 @@ Put the JSON
 """
 from pathlib import Path
 
-from pygit2 import Repository, _pygit2
+from pygit2 import Repository, _pygit2, GIT_SORT_TIME
+
+
+def list_commits_between(repo, target_commit, base_commit):
+    start = repo.revparse_single(target_commit)
+    stop = repo.revparse_single(base_commit)
+
+    commits = []
+
+    walker = repo.walk(start.id, GIT_SORT_TIME)
+    walker.hide(stop.id)
+    for commit in walker:
+        commits.append(str(commit.id))
+
+    return commits
+
+
+def repo_at(root):
+    try:
+        repo = Repository(root + '.git')
+    except _pygit2.GitError:
+        return None
+    return repo
+
 
 REPO_ROOT = "/test_src/"
 
@@ -26,15 +49,31 @@ def test_repo_is_present_in_image():
     assert repo_path.is_dir()
 
 
-def is_repo(root):
-    try:
-        repo = Repository(root + '.git')
-    except _pygit2.GitError:
-        return False
-    return True
-
-
 def test_is_repo():
-    assert is_repo(REPO_ROOT)
-    assert not is_repo("/cdb_data/")
+    assert repo_at(REPO_ROOT) is not None
+    assert repo_at("/cdb_data/") is None
 
+
+def test_list_commits_between_master_and_production():
+    commits = list_commits_between(repo_at(REPO_ROOT), "master", "production")
+    expected = [
+        "8f5b384644eb83e7f2a6d9499539a077e7256b8b",
+        "e0ad84e1a2464a9486e777c1ecde162edff930a9"]
+    assert commits == expected
+
+
+def test_list_commits_between_release_branch_and_production():
+    commits = list_commits_between(repo_at(REPO_ROOT), "release-branch", "master")
+    expected = [
+        "e0d1acf1adb9e263c1b6e0cfe3e0d2c1ade371e1"]
+    assert commits == expected
+
+
+def test_list_commits_between_master_and_commit():
+    commits = list_commits_between(repo_at(REPO_ROOT), "master", "b7e6aa63087fcb1e64a5f2a99c8d255415d8cb99")
+    expected = [
+        "8f5b384644eb83e7f2a6d9499539a077e7256b8b",
+        "e0ad84e1a2464a9486e777c1ecde162edff930a9",
+        "b6c9e60f281e37d912ec24f038b7937f79723fb4"
+    ]
+    assert commits == expected
