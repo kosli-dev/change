e553ab66535cab0098ca3a68164825ef65ec7643
Mike Long <mike@compliancedb.com>
2020-10-30 12:03:11 +0100

    Cleanup

--- tests/test_cdb_utils.py
+++ tests/test_cdb_utils.py
@@ -1,13 +1,6 @@
 import os
 
 from cdb.cdb_utils import load_user_data
-from cdb.put_artifact_image import put_artifact_image
-
-# def test_put_dry_run_doesnt_call(mocker):
-#     requests = mocker.patch('cdb.http.req')
-#     os.environ["CDB_DRY_RUN"] = "TRUE"
-#     put_payload({}, "https://www.example.com", "")
-#     requests.put.assert_not_called()
 
 
 def test_load_user_data_None_if_not_in_env(mocker):
@@ -27,7 +20,4 @@ def test_load_user_data_returns_object_from_file(mocker):
     assert user_data == {"super_cool": "user data"}
     mock_json.load.assert_called_once_with("/some/random/file.json")
 
-
-# def test_put_artifact_image():
-#     put_artifact_image("test-pipeline.json")
 
