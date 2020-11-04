c92f0ec69a55914d15f7d448901f756b6b91199a
Neethu Philip <neethuphilip@Neethus-MacBook-Pro.local>
2020-11-04 15:34:00 +0100

    Updated default value of env.get to String type

--- cdb/create_deployment.py
+++ cdb/create_deployment.py
@@ -9,9 +9,9 @@ from cdb.http import http_post_payload
 
 def create_deployment_payload(env=os.environ):
     deployment = {}
-    deployment["artifact_sha256"] = env.get('CDB_ARTIFACT_SHA', None)
-    deployment["environment"] = env.get('CDB_ENVIRONMENT', None)
-    deployment["description"] = env.get('CDB_DESCRIPTION', None)
+    deployment["artifact_sha256"] = env.get('CDB_ARTIFACT_SHA', 'None')
+    deployment["environment"] = env.get('CDB_ENVIRONMENT', 'None')
+    deployment["description"] = env.get('CDB_DESCRIPTION', 'None')
 
     if env.get("CDB_USER_DATA"):
         deployment["user_data"] = load_user_data()
