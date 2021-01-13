5700d57a8cd76b5599705d87cec11366cd95d7a4
Jon Jagger <jon@jaggersoft.com>
2021-01-13 17:21:47 +0000

    Refactor with statements (in tests) into a single with statement

--- tests/test_http.py
+++ tests/test_http.py
@@ -5,15 +5,13 @@ from tests.flushing_capsys import flushing_capsys
 
 def test_put_dry_run_doesnt_call(mocker, capsys):
     requests = mocker.patch('cdb.http.req')
-    with flushing_capsys(capsys):
-        with cdb_dry_run():
-            http_put_payload("https://www.example.com", {}, "")
+    with flushing_capsys(capsys), cdb_dry_run():
+        http_put_payload("https://www.example.com", {}, "")
     requests.put.assert_not_called()
 
 
 def test_post_dry_run_doesnt_call(mocker, capsys):
     requests = mocker.patch('cdb.http.req')
-    with flushing_capsys(capsys):
-        with cdb_dry_run():
-            http_post_payload("https://www.example.com", {}, "")
+    with flushing_capsys(capsys), cdb_dry_run():
+        http_post_payload("https://www.example.com", {}, "")
     requests.post.assert_not_called()
