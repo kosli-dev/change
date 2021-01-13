2d0e51c9939a99f84885e2165006a4b26772d63d
Jon Jagger <jon@jaggersoft.com>
2021-01-13 09:00:12 +0000

    In http-retry test, monkey-patch the backoff otherwise test takes 30 seconds to run.

--- integration_tests/test_http_retry.py
+++ integration_tests/test_http_retry.py
@@ -3,7 +3,7 @@ import requests
 
 from cdb.create_approval import create_approval
 from tests.test_git import TEST_REPO_ROOT
-from cdb.http_retry import LoggingRetry
+import cdb.http_retry
 
 """
 At first this tried to use the responses library to stub the http calls.
@@ -14,7 +14,7 @@ See https://github.com/getsentry/responses/issues/135
 
 
 def test_total_retry_time_is_about_30_seconds():
-    assert LoggingRetry().total_backoff_time() == 31  # 1+2+4+8+16
+    assert cdb.http_retry.LoggingRetry().total_backoff_time() == 31  # 1+2+4+8+16
 
 
 @httpretty.activate
@@ -41,8 +41,8 @@ def test_503_post_retries_5_times(capsys):
         "CDB_SRC_REPO_ROOT": TEST_REPO_ROOT,
     }
     try:
-        # The retry sleep delays are [ 0, 2, 4, 8, 16 ]
-        # So this test will take ~30 seconds to pass :-(
+        original_backoff_factor = cdb.http_retry.RETRY_BACKOFF_FACTOR
+        cdb.http_retry.RETRY_BACKOFF_FACTOR = 0.001  # otherwise test takes 30 seconds
         create_approval("integration_tests/test-pipefile.json", env=env)
     except requests.exceptions.RetryError:
         captured = capsys.readouterr()
@@ -51,10 +51,12 @@ def test_503_post_retries_5_times(capsys):
         # request.url drops the protocol and hostname :(
         assert "URL={}".format(route) in stdout
         assert "STATUS=503" in stdout
-        assert "Retrying in 2 seconds (1/4)" in stdout
-        assert "Retrying in 4 seconds (2/4)" in stdout
-        assert "Retrying in 8 seconds (3/4)" in stdout
-        assert "Retrying in 16 seconds (4/4)" in stdout
+        assert "Retrying in 0.002 seconds (1/4)" in stdout
+        assert "Retrying in 0.004 seconds (2/4)" in stdout
+        assert "Retrying in 0.008 seconds (3/4)" in stdout
+        assert "Retrying in 0.016 seconds (4/4)" in stdout
         assert len(httpretty.latest_requests()) == 5+1
-
+    finally:
+        cdb.http_retry.RETRY_BACKOFF_FACTOR = original_backoff_factor
+        assert cdb.http_retry.RETRY_BACKOFF_FACTOR != 0.001
 
