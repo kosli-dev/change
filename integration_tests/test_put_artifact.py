025441dae7941e3d11df4d3a0dd9a381421701b5
Jon Jagger <jon@jaggersoft.com>
2021-01-18 12:18:54 +0000

    Move SetEnvVars context-manager test-helper class into its own module

--- integration_tests/test_put_artifact.py
+++ integration_tests/test_put_artifact.py
@@ -1,9 +1,9 @@
 from cdb.put_artifact import put_artifact
-
 from os import environ
 from approvaltests.approvals import verify
 from approvaltests.reporters import PythonNativeReporter
 from tests.cdb_dry_run import cdb_dry_run
+from tests.set_env_vars import SetEnvVars
 
 
 def test_message_when_env_var_CDB_ARTIFACT_FILENAME_is_missing(capsys):
@@ -15,28 +15,12 @@ def test_message_when_env_var_CDB_ARTIFACT_FILENAME_is_missing(capsys):
 
 
 def test_message_when_env_var_CDB_ARTIFACT_SHA_is_missing(capsys):
-    env_vars = {
+    env = {
         "CDB_ARTIFACT_FILENAME": "tests_data/put-artifact.txt",
         "CDB_ARTIFACT_SHA": "UNDEFINED"
     }
-    with cdb_dry_run(), SetEnv(env_vars):
+    with cdb_dry_run(), SetEnvVars(env):
         put_artifact("integration_tests/test-pipefile.json")
 
     captured = capsys.readouterr()
     verify(captured.out, PythonNativeReporter())
-
-
-class SetEnv(object):
-    def __init__(self, env_vars):
-        self._env_vars = env_vars
-        for (name, value) in env_vars.items():
-            environ[name] = value
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, _type, _value, _traceback):
-        for name in self._env_vars:
-            environ.pop(name)
-
-
---
+++ tests/set_env_vars.py
@@ -0,0 +1,16 @@
+from os import environ
+
+
+class SetEnvVars(object):
+    def __init__(self, env_vars):
+        self._env_vars = env_vars
+        for (name, value) in env_vars.items():
+            environ[name] = value
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, _type, _value, _traceback):
+        for name in self._env_vars:
+            environ.pop(name)
+
