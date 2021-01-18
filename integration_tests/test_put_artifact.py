49834387ee629106cbe162e85da3186522c9c46d
Jon Jagger <jon@jaggersoft.com>
2021-01-18 12:10:26 +0000

    Collapse env-var setting duplicaiton in put-artifact approval-test

--- integration_tests/test_put_artifact.py
+++ integration_tests/test_put_artifact.py
@@ -15,10 +15,12 @@ def test_message_when_env_var_CDB_ARTIFACT_FILENAME_is_missing(capsys):
 
 
 def test_message_when_env_var_CDB_ARTIFACT_SHA_is_missing(capsys):
-    with cdb_dry_run():
-        with SetEnv({"CDB_ARTIFACT_FILENAME": "tests_data/put-artifact.txt"}):
-            with SetEnv({"CDB_ARTIFACT_SHA": "UNDEFINED"}):
-                put_artifact("integration_tests/test-pipefile.json")
+    env_vars = {
+        "CDB_ARTIFACT_FILENAME": "tests_data/put-artifact.txt",
+        "CDB_ARTIFACT_SHA": "UNDEFINED"
+    }
+    with cdb_dry_run(), SetEnv(env_vars):
+        put_artifact("integration_tests/test-pipefile.json")
 
     captured = capsys.readouterr()
     verify(captured.out, PythonNativeReporter())
