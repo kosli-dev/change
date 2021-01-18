78a51feaf5968824c27c557676fddeeb8ee8b7ce
Jon Jagger <jon@jaggersoft.com>
2021-01-18 14:19:30 +0000

    Refactor put-artifact test to use non-empty artifact file with better name

--- integration_tests/test_put_artifact.py
+++ integration_tests/test_put_artifact.py
@@ -12,7 +12,7 @@ def test_message_when_env_var_CDB_ARTIFACT_FILENAME_is_missing(capsys):
 
 def test_message_when_env_var_CDB_ARTIFACT_SHA_is_missing(capsys):
     env = {
-        "CDB_ARTIFACT_FILENAME": "tests_data/put-artifact.txt",
+        "CDB_ARTIFACT_FILENAME": "tests_data/coverage.txt",
         "CDB_ARTIFACT_SHA": "UNDEFINED"
     }
     with cdb_dry_run(), AutoEnvVars(env):
