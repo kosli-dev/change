543e6a876bb05a7a8d9b20cd8309f2dd088ebc9f
Jon Jagger <jon@jaggersoft.com>
2021-01-19 13:46:06 +0000

    Removed debug prints from test

--- integration_tests/test_put_artifact_image.py
+++ integration_tests/test_put_artifact_image.py
@@ -1,18 +1,11 @@
-import os
 import pytest
 from cdb.put_artifact_image import put_artifact_image
 
 from tests.utils import AutoEnvVars, cdb_dry_run, verify_approval
 
 
-@pytest.mark.skip(reason="investigating env-vars being set in other tests")
-def test_message_when_no_env_vars(): #capsys):
-
-    print("X"*60)
-    for name,value in os.environ.items():
-        print(name+"="+value)
-    print("X"*60)
-
+@pytest.mark.skip(reason="pending while investigating env-vars state being set in other tests")
+def test_message_when_no_env_vars(capsys):
     with cdb_dry_run():
         put_artifact_image("integration_tests/test-pipefile.json")
 
