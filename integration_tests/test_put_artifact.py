da616c7b3ed792612874ebaa89f7af9a009b3ebf
Jon Jagger <jon@jaggersoft.com>
2021-01-20 08:24:18 +0000

    Refactor; rename variable

--- integration_tests/test_put_artifact.py
+++ integration_tests/test_put_artifact.py
@@ -26,14 +26,14 @@ def test_message_when_env_var_CDB_ARTIFACT_SHA_is_not_defined(capsys):
         "CDB_ARTIFACT_FILENAME": "tests_data/coverage.txt",
     }
 
-    with cdb_dry_run(), AutoEnvVars(env) as ev_context:
+    with cdb_dry_run(), AutoEnvVars(env) as env_context:
         put_artifact("integration_tests/test-pipefile.json")
 
     expected_new_env_vars = {
         "CDB_ARTIFACT_SHA": "ccee89ccdc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc",
         "CDB_ARTIFACT_FILENAME": "tests_data/coverage.txt"
     }
-    actual_new_env_vars = ev_context.new_env_vars()
+    actual_new_env_vars = env_context.new_env_vars()
     assert expected_new_env_vars == actual_new_env_vars
 
     verify_approval(capsys, ["out"])
