adc06378da6834962de51242c2151ca6b9cca9bd
Arstan <arstanrys@gmail.com>
2021-01-21 22:13:07 +0100

    Add first approval-test for control_junit()

---
+++ tests/integration/test_control_junit.py
@@ -0,0 +1,22 @@
+from cdb.control_junit import control_junit
+import os
+import docker
+from pytest import raises
+from tests.utils import AutoEnvVars, cdb_dry_run, verify_approval
+
+
+def test_message_when_no_env_vars(capsys):
+    with cdb_dry_run(), AutoEnvVars(), raises(docker.errors.DockerException):
+        control_junit("tests/integration/test-pipefile.json")
+
+    verify_approval(capsys, ["out"])
+
+def test_message_when_CDB_TEST_RESULTS_DIR_defined(capsys):
+    env = {
+        "CDB_TEST_RESULTS_DIR": "tests/integration/"
+    }
+
+    with cdb_dry_run(), AutoEnvVars(env), raises(docker.errors.DockerException):
+        control_junit("tests/integration/test-pipefile.json")
+
+    verify_approval(capsys, ["out"])
