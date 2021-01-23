0c29415db5095b3099f7a927a1c25b739c3281b1
Jon Jagger <jon@jaggersoft.com>
2021-01-23 12:50:42 +0000

    Refactor AutoEnvVars() with better names

--- tests/unit/test_auto_env_vars.py
+++ tests/unit/test_auto_env_vars.py
@@ -4,46 +4,43 @@ from tests.utils import AutoEnvVars, UnexpectedEnvVarsError
 from pytest import raises
 
 
-def test_env_vars_set_before_the_with_statement_are_unaffected():
-    os.environ["YOU_SEE"] = "Nature"
-    env = {
-        "YOU_SEE": "Wildlife"
-    }
-    with AutoEnvVars(env):
-        assert os.getenv("YOU_SEE") == "Wildlife"
+def test_new_env_vars_are_available_only_inside_the_with_statement():
+    assert os.getenv("NEW_ENV_VAR") is None
 
-    assert os.getenv("YOU_SEE") == "Nature"
+    env = { "NEW_ENV_VAR": "Fishing" }
+    with AutoEnvVars(env):
+        assert os.getenv("NEW_ENV_VAR") == "Fishing"
+
+    assert os.getenv("NEW_ENV_VAR") is None
+
+
+def test_env_vars_that_exist_before_the_with_statement_still_exist_after_the_exit():
+    os.environ["EXISTING_ENV_VAR"] = "Wonderland"
+
+    with AutoEnvVars({}):
+        assert os.getenv("EXISTING_ENV_VAR") == "Wonderland"
+
+    assert os.getenv("EXISTING_ENV_VAR") == "Wonderland"
 
 
 def test_new_env_vars_set_inside_with_statement_must_be_specified_in_the_second_init_arg_and_are_unavailable_after_the_with_statement():
-    assert os.getenv("GETS_YOU") is None
-    env = {
-        "GETS_YOU": "Outside"
-    }
+    assert os.getenv("ENV_SET_INSIDE_WITH") is None
+
+    env = { "ENV_SET_INSIDE_WITH": "Humpty" }
     with AutoEnvVars({}, env):
-        os.environ["GETS_YOU"] = "Outside"
-        assert os.getenv("GETS_YOU") == "Outside"
+        os.environ["ENV_SET_INSIDE_WITH"] = "Humpty"
+        assert os.getenv("ENV_SET_INSIDE_WITH") == "Humpty"
 
-    assert os.getenv("GETS_YOU") is None
+    assert os.getenv("ENV_SET_INSIDE_WITH") is None
 
 
-def test_new_env_var_set_inside_with_statement_not_specified_in_second_init_arg_raises_exception():
-    assert os.getenv("ALPHA") is None
+def test_new_env_var_set_inside_with_statement_not_specified_in_arg2_raises_exception():
+    assert os.getenv("ENV_VAR_NOT_IN_ARG2") is None
 
     with raises(UnexpectedEnvVarsError) as exc:
         with AutoEnvVars({}, {}):
-            os.environ["ALPHA"] = "42"
+            os.environ["ENV_VAR_NOT_IN_ARG2"] = "42"
 
-    assert os.getenv("ALPHA") is None
+    assert os.getenv("ENV_VAR_NOT_IN_ARG2") is None
 
 
-def test_new_env_vars_passed_in_init_are_available_only_inside_the_with_statement():
-    assert os.getenv("NICE_HOBBY") is None
-    env = {
-        "NICE_HOBBY": "Fishing"
-    }
-
-    with AutoEnvVars(env):
-        assert os.getenv("NICE_HOBBY") == "Fishing"
-
-    assert os.getenv("NICE_HOBBY") is None
