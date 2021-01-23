6c3d134862592d81e4ccc47055876cad4e235e64
Jon Jagger <jon@jaggersoft.com>
2021-01-23 09:31:18 +0000

    Update test name

--- tests/integration/test_put_artifact_image.py
+++ tests/integration/test_put_artifact_image.py
@@ -5,6 +5,10 @@ from pytest import raises
 from tests.utils import AutoEnvVars, cdb_dry_run
 
 
-def test_message_when_no_env_vars():
+def test_when_no_env_vars_raises_DockerException():
+    """
+    This is not the desired behaviour, but until more tests are
+    in place we are not refactoring.
+    """
     with cdb_dry_run(), AutoEnvVars(), raises(docker.errors.DockerException):
         put_artifact_image("tests/integration/test-pipefile.json")
