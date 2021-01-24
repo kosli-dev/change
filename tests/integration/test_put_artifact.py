403851da761a7796da56afdc458980e706ec8dc9
Jon Jagger <jon@jaggersoft.com>
2021-01-24 09:33:18 +0000

    Add test for put_artifact() which defines CDB_ARTIFACT_SHA env-var

--- tests/integration/test_put_artifact.py
+++ tests/integration/test_put_artifact.py
@@ -34,3 +34,17 @@ def test_message_when_env_var_CDB_ARTIFACT_SHA_is_not_defined(capsys):
         put_artifact("tests/integration/test-pipefile.json")
 
     verify_approval(capsys, ["out"])
+
+
+def test_message_when_env_var_CDB_ARTIFACT_SHA_is_defined(capsys):
+    env = {
+        "CDB_ARTIFACT_FILENAME": "tests/data/coverage.txt",
+        "CDB_ARTIFACT_SHA": "ccee89ccdc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
+    }
+
+    set_env_vars = {}
+
+    with AutoEnvVars({**CDB_DRY_RUN, **env}, set_env_vars):
+        put_artifact("tests/integration/test-pipefile.json")
+
+    verify_approval(capsys, ["out"])
