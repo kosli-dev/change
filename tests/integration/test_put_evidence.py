ff8dbd29654419300d36bc4b8440ee1b4d1a7bb1
Jon Jagger <jon@jaggersoft.com>
2021-01-24 20:40:33 +0000

    Add approval-test for put_evidence() with required env-vars defined

--- tests/integration/test_put_evidence.py
+++ tests/integration/test_put_evidence.py
@@ -5,6 +5,24 @@ from pytest import raises
 from tests.utils import AutoEnvVars, CDB_DRY_RUN, verify_approval
 
 
+def test_when_all_env_vars_defined_artifact_sha_comes_from_CDB_ARTIFACT_SHA(capsys):
+    env = {
+        "CDB_HOST": "http://test.compliancedb.com",
+        "CDB_API_TOKEN": "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
+        # "CDB_ARTIFACT_DOCKER_IMAGE": "acme/widget:5.6",
+        "CDB_ARTIFACT_SHA": "a7cdaef69c676c2466571d3233380d559ccc2032b258fc5e73f99a103db462ef",
+        "CDB_IS_COMPLIANT": "TRUE",
+        "CDB_EVIDENCE_TYPE": "coverage",
+        "CDB_DESCRIPTION": "branch coverage",
+        "CDB_BUILD_NUMBER": "19",
+        "CDB_CI_BUILD_URL": "https://gitlab/build/1456",
+    }
+    set_env_vars = {}
+    with AutoEnvVars({**CDB_DRY_RUN, **env}, set_env_vars):
+        put_evidence("tests/integration/test-pipefile.json")
+    verify_approval(capsys, ["out"])
+
+
 def test_when_no_env_vars_raises_DockerException():
     set_env_vars = {}
     with AutoEnvVars(CDB_DRY_RUN, set_env_vars), raises(docker.errors.DockerException):
