db02136d814d886e3fc772390765a145e2694a27
Jon Jagger <jon@jaggersoft.com>
2021-01-24 10:11:45 +0000

    Refactor tests/integration/test_control_junit.py to use CDB_DRY_RUN as part of AutoEnvVars()

--- tests/integration/test_control_junit.py
+++ tests/integration/test_control_junit.py
@@ -2,23 +2,23 @@ from cdb.control_junit import control_junit
 import os
 import docker
 from pytest import raises
-from tests.utils import AutoEnvVars, cdb_dry_run, verify_approval
+from tests.utils import AutoEnvVars, CDB_DRY_RUN, verify_approval
 
 
 def test_message_when_no_env_vars(capsys):
-    env = {}
+    env = CDB_DRY_RUN
     set_env_vars = {}
-    with cdb_dry_run(), AutoEnvVars(env, set_env_vars), raises(docker.errors.DockerException):
+    with AutoEnvVars(env, set_env_vars), raises(docker.errors.DockerException):
         control_junit("tests/integration/test-pipefile.json")
 
     verify_approval(capsys, ["out"])
 
 
 def test_message_when_CDB_TEST_RESULTS_DIR_defined(capsys):
-    env = { "CDB_TEST_RESULTS_DIR": "tests/integration/" }
+    env = {"CDB_TEST_RESULTS_DIR": "tests/integration/"}
     set_env_vars = {}
 
-    with cdb_dry_run(), AutoEnvVars(env, set_env_vars), raises(docker.errors.DockerException):
+    with AutoEnvVars({**CDB_DRY_RUN, **env}, set_env_vars), raises(docker.errors.DockerException):
         control_junit("tests/integration/test-pipefile.json")
 
     verify_approval(capsys, ["out"])
