c27b4d38413e85bb02ad83825dc7e60ee682d81c
Jon Jagger <jon@jaggersoft.com>
2021-01-24 09:49:20 +0000

    Add approval-test for put_artifact_image() using CDB_ARTIFACT_SHA env-var

--- tests/integration/test_put_artifact_image.py
+++ tests/integration/test_put_artifact_image.py
@@ -2,7 +2,7 @@ from cdb.put_artifact_image import put_artifact_image
 import docker
 
 from pytest import raises
-from tests.utils import AutoEnvVars, CDB_DRY_RUN
+from tests.utils import AutoEnvVars, CDB_DRY_RUN, verify_approval
 
 
 def test_when_no_env_vars_raises_DockerException():
@@ -12,3 +12,16 @@ def test_when_no_env_vars_raises_DockerException():
     """
     with AutoEnvVars(CDB_DRY_RUN), raises(docker.errors.DockerException):
         put_artifact_image("tests/integration/test-pipefile.json")
+
+
+def test_when_CDB_ARTIFACT_SHA_is_defined(capsys):
+    env = {
+        "CDB_ARTIFACT_SHA": "ccee89ccdc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
+    }
+
+    set_env_vars = {}
+
+    with AutoEnvVars({**CDB_DRY_RUN, **env}, set_env_vars):
+        put_artifact_image("tests/integration/test-pipefile.json")
+
+    verify_approval(capsys, ["out"])
