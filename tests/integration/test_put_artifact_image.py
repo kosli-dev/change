895aac2d89f5af734fc748e9dbef7780e7561ef0
Jon Jagger <jon@jaggersoft.com>
2021-01-25 08:10:11 +0000

    Rename put_artifact_image tests with shorter names for easier to use approval files

--- tests/integration/test_put_artifact_image.py
+++ tests/integration/test_put_artifact_image.py
@@ -5,7 +5,8 @@ from pytest import raises
 from tests.utils import AutoEnvVars, CDB_DRY_RUN, verify_approval
 
 
-def test_when_all_env_vars_defined_artifact_sha_from_CDB_ARTIFACT_SHA(capsys):
+def test_all_env_vars_defined(capsys):
+    # artifact sha comes from CDB_ARTIFACT_SHA
     env = {
         "CDB_HOST": "http://test.compliancedb.com",
         "CDB_API_TOKEN": "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
@@ -22,7 +23,7 @@ def test_when_all_env_vars_defined_artifact_sha_from_CDB_ARTIFACT_SHA(capsys):
     verify_approval(capsys, ["out"])
 
 
-def test_when_no_env_vars_raises_DockerException():
+def test_no_env_vars_raises_DockerException():
     """
     This is not the desired behaviour, but until more tests are
     in place we are not refactoring.
@@ -32,11 +33,3 @@ def test_when_no_env_vars_raises_DockerException():
         put_artifact_image("tests/integration/test-pipefile.json")
 
 
-def test_when_CDB_ARTIFACT_SHA_is_defined(capsys):
-    env = {
-        "CDB_ARTIFACT_SHA": "ccee89ccdc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
-    }
-    set_env_vars = {}
-    with AutoEnvVars({**CDB_DRY_RUN, **env}, set_env_vars):
-        put_artifact_image("tests/integration/test-pipefile.json")
-    verify_approval(capsys, ["out"])
