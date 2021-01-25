5806a6300d78226200b6c57c9d80321e342fb72c
Jon Jagger <jon@jaggersoft.com>
2021-01-25 08:19:00 +0000

    Update approval-test for create_deployment() to use all required env-vars

--- tests/integration/test_create_deployment.py
+++ tests/integration/test_create_deployment.py
@@ -3,10 +3,12 @@ from cdb.create_deployment import create_deployment
 from tests.utils import AutoEnvVars, CDB_DRY_RUN, verify_approval
 
 
-# This is setting the CDB_ARTIFACT_SHA env-var
-def test_CDB_ARTIFACT_SHA_is_defined(capsys):
+def test_all_env_vars_uses_CDB_ARTIFACT_SHA(capsys):
     env = {
+        "CDB_HOST": "http://test.compliancedb.com",
+        "CDB_API_TOKEN": "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
         "CDB_ARTIFACT_SHA": '1234567890',
+        "CDB_ENVIRONMENT": "production",
     }
     set_env_vars = {}
     with AutoEnvVars({**CDB_DRY_RUN, **env}, set_env_vars):
