682fb0f05d5d672aa24318497452d15d92614052
Arstan <arstanrys@gmail.com>
2021-01-25 12:26:04 +0100

    Add second approval-test for put_artifact_image(). Add CDB_ARTIFACT_SHA to its README.md

--- README.md
+++ README.md
@@ -101,13 +101,14 @@ docker run --rm --name comply \
         compliancedb/cdb_controls python -m cdb.put_artifact_image -p /data/pipeline.json
 ```
 
-This command expect the following environment variables:
+This command expects the following environment variables:
 
 | VARIABLE | Requirement | Description |
 |------|-----|-----|
 | CDB_HOST | Optional | The host name for ComplianceDB, default is https://app.compliancedb.com |
 | CDB_API_TOKEN | Required | Your API token for ComplianceDB |
 | CDB_ARTIFACT_DOCKER_IMAGE | Required | The resulting docker image |
+| CDB_ARTIFACT_SHA | Optional | The SHA256 for the artifact |
 | CDB_IS_COMPLIANT | Required | Whether this artifact is considered compliant from you build process |
 | CDB_ARTIFACT_GIT_URL | Required | Link to the source git commit this build was based on |
 | CDB_ARTIFACT_GIT_COMMIT | Required | The sha of the git commit that produced this build |
--- tests/integration/test_put_artifact_image.py
+++ tests/integration/test_put_artifact_image.py
@@ -23,6 +23,25 @@ def test_all_env_vars_defined(capsys):
     verify_approval(capsys, ["out"])
 
 
+def test_all_env_vars_when_CDB_ARTIFACT_SHA_not_defined(capsys, mocker):
+    # artifact sha comes from CDB_ARTIFACT_SHA
+    env = {
+        "CDB_HOST": "http://test.compliancedb.com",
+        "CDB_API_TOKEN": "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
+        "CDB_ARTIFACT_DOCKER_IMAGE": "acme/widget:3.4",
+        "CDB_IS_COMPLIANT": "TRUE",
+        "CDB_ARTIFACT_GIT_URL": "http://github/me/project/commit/12037940e4e7503055d8a8eea87e177f04f14616",
+        "CDB_ARTIFACT_GIT_COMMIT": "92037940e4e7503055d8a8eea87e177f04f14616",
+        "CDB_CI_BUILD_URL": "https://gitlab/build/5641"
+    }
+    set_env_vars = {}
+    with AutoEnvVars({**CDB_DRY_RUN, **env}, set_env_vars):
+        sha = "a7cdaef69c676c2466571d3233380d559ccc2032b258fc5e73f99a103db462ef"
+        mocker.patch('cdb.cdb_utils.calculate_sha_digest_for_docker_image', return_value=sha)
+        put_artifact_image("tests/integration/test-pipefile.json")
+    verify_approval(capsys, ["out"])
+
+
 def test_no_env_vars_raises_DockerException():
     """
     This is not the desired behaviour, but until more tests are
