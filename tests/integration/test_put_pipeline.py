1d0684956826e8a819e83a5f7194aecb7af3d54a
Jon Jagger <jon@jaggersoft.com>
2021-01-25 15:41:44 +0000

    Add approval-tests for put_pipeline() one with required env-vars, one with all env-vars

--- tests/integration/test_put_pipeline.py
+++ tests/integration/test_put_pipeline.py
@@ -3,9 +3,19 @@ from cdb.put_pipeline import put_pipeline
 from tests.utils import AutoEnvVars, CDB_DRY_RUN, verify_approval
 
 
-def test_put_pipeline(capsys):
+def test_required_env_vars(capsys):
     env = {
-        "CDB_HOST": "http://app2.compliancedb.com",
+        "CDB_API_TOKEN": "SOME_RANDOM_TOKEN",
+    }
+    set_env_vars = {}
+    with AutoEnvVars(CDB_DRY_RUN, set_env_vars):
+        put_pipeline("tests/integration/test-pipefile.json", env)
+    verify_approval(capsys)
+
+
+def test_all_env_vars(capsys):
+    env = {
+        "CDB_HOST": "http://test.compliancedb.com",
         "CDB_API_TOKEN": "SOME_RANDOM_TOKEN",
     }
     set_env_vars = {}
