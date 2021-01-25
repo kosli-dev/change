ed84fc329d16f2db8e4b10be19def02d1dc7142a
Jon Jagger <jon@jaggersoft.com>
2021-01-25 08:27:45 +0000

    Remove unneeded control_junit ...approved.txt files

--- tests/integration/test_control_junit.py
+++ tests/integration/test_control_junit.py
@@ -4,20 +4,11 @@ from pytest import raises
 from tests.utils import AutoEnvVars, CDB_DRY_RUN, verify_approval
 
 
-def test_message_when_no_env_vars(capsys):
+def test_no_env_vars():
     env = CDB_DRY_RUN
     set_env_vars = {}
     with AutoEnvVars(env, set_env_vars), raises(docker.errors.DockerException):
         control_junit("tests/integration/test-pipefile.json")
 
-    verify_approval(capsys, ["out"])
 
 
-def test_message_when_CDB_TEST_RESULTS_DIR_defined(capsys):
-    env = {"CDB_TEST_RESULTS_DIR": "tests/integration/"}
-    set_env_vars = {}
-
-    with AutoEnvVars({**CDB_DRY_RUN, **env}, set_env_vars), raises(docker.errors.DockerException):
-        control_junit("tests/integration/test-pipefile.json")
-
-    verify_approval(capsys, ["out"])
