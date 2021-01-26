eb5b1b086dc5c31c98a1dc5e6c74d3084452d108
Jon Jagger <jon@jaggersoft.com>
2021-01-26 11:00:59 +0000

    Update approval-test for put_artifact() with mocked sha from CDB_ARTIFACT_FILENAME

--- tests/integration/test_put_artifact.py
+++ tests/integration/test_put_artifact.py
@@ -3,22 +3,23 @@ from cdb.put_artifact import put_artifact
 from tests.utils import AutoEnvVars, CDB_DRY_RUN, verify_approval, auto_reading
 
 
-def test_all_env_vars_uses_CDB_ARTIFACT_FILENAME(capsys):
+def test_required_env_vars_uses_CDB_ARTIFACT_FILENAME(capsys, mocker):
     # Does not provide CDB_ARTIFACT_SHA
     # Assumption is that CDB_ARTIFACT_FILENAME names a file
     # that is volume-mounted and the CDB_ARTIFACT_SHA is calculated.
     env = {
-        "CDB_HOST": "http://test.compliancedb.com",
         "CDB_API_TOKEN": "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
-        "CDB_ARTIFACT_FILENAME": "tests/data/coverage.txt",
+        "CDB_ARTIFACT_FILENAME": "src/data/robocop.xml",
         "CDB_IS_COMPLIANT": "TRUE",
         "CDB_ARTIFACT_GIT_URL": "http://github/me/project/commit/abc50c8a53f79974d615df335669b59fb56a4ed3",
         "CDB_ARTIFACT_GIT_COMMIT": "abc50c8a53f79974d615df335669b59fb56a4ed3",
         "CDB_CI_BUILD_URL": "https://gitlab/build/1456",
         "CDB_BUILD_NUMBER": "349"
     }
-    set_env_vars = {'CDB_ARTIFACT_SHA': 'ccee89ccdc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc'}
+    sha = "ddcdaef69c676c2466571d3288880d559ccc2032b258fc5e73f99a103db462ee"
+    set_env_vars = {'CDB_ARTIFACT_SHA': sha}
     with AutoEnvVars({**CDB_DRY_RUN, **env}, set_env_vars):
+        mocker.patch('cdb.cdb_utils.calculate_sha_digest_for_file', return_value=sha)
         put_artifact("tests/integration/test-pipefile.json")
     verify_approval(capsys, ["out"])
 
