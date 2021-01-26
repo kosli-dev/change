d8a674befb5053a50bcd823c12fc9579baa222b7
Jon Jagger <jon@jaggersoft.com>
2021-01-26 14:34:48 +0000

    Added first approval-test for bitbucket put_pipeline

--- tests/bb_integration/test_put_pipeline.py
+++ tests/bb_integration/test_put_pipeline.py
@@ -1,24 +1,19 @@
 from cdb.put_pipeline import put_pipeline
+from pipe import BitbucketPipe, schema
 
 from tests.utils import AutoEnvVars, CDB_DRY_RUN, verify_approval
 
 
 def test_required_env_vars(capsys):
     env = {
+        "CDB_COMMAND": "put_pipeline",
+        "CDB_PIPELINE_DEFINITION": "tests/data/pipefile.json",
         "CDB_API_TOKEN": "SOME_RANDOM_TOKEN",
     }
     set_env_vars = {}
-    with AutoEnvVars(CDB_DRY_RUN, set_env_vars):
-        put_pipeline("tests/integration/test-pipefile.json", env)
+    with AutoEnvVars({**CDB_DRY_RUN, **env}, set_env_vars):
+        pipe = BitbucketPipe(pipe_metadata='/pipe.yml', schema=schema)
+        pipe.run()
     verify_approval(capsys)
 
 
-def test_all_env_vars(capsys):
-    env = {
-        "CDB_HOST": "http://test.compliancedb.com",  # optional
-        "CDB_API_TOKEN": "SOME_RANDOM_TOKEN",
-    }
-    set_env_vars = {}
-    with AutoEnvVars(CDB_DRY_RUN, set_env_vars):
-        put_pipeline("tests/integration/test-pipefile.json", env)
-    verify_approval(capsys)
