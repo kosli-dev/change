5329210b64ca7af4c8e07cb6b37711377fdd7ae6
Jon Jagger <jon@jaggersoft.com>
2021-01-27 11:11:24 +0000

    Add approval-test for bitbucket control_junit() using CDB_TEST_RESULTS_DIR that exists

--- tests/bb_integration/test_bb_control_junit.py
+++ tests/bb_integration/test_bb_control_junit.py
@@ -4,6 +4,8 @@ from tests.utils import AutoEnvVars, CDB_DRY_RUN, verify_approval
 
 
 def test_required_env_vars(capsys, mocker):
+    # BITBUCKET_COMMIT and BITBUCKET_BUILD_NUMBER are required
+    # If they are not set the code tries to set an env-var to none
     # BITBUCKET_WORKSPACE probably should be required, but None ends up in the url
     # BITBUCKET_REPO_SLUG probably should be required, but None ends up in the url
     # CDB_TEST_RESULTS_DIR has a default, '/data/junit/' but this is _not_ checked
@@ -30,26 +32,25 @@ def test_required_env_vars(capsys, mocker):
     verify_approval(capsys)
 
 
-def test_intended_env_vars(capsys, mocker):
-    # BITBUCKET_COMMIT and BITBUCKET_BUILD_NUMBER are required
-    # If they are not set the code tries to set an env-var to none
-    bitbucket_commit = "abc50c8a53f79974d615df335669b59fb56a4ed3"
+def test_all_env_vars(capsys, mocker):
+    bitbucket_commit = "dad50c8a53f79974d615df335669b59fb56a4ed3"
     env = {
         "CDB_COMMAND": "control_junit",
         "CDB_PIPELINE_DEFINITION": "tests/data/pipefile.json",
         "CDB_API_TOKEN": "SOME_RANDOM_TOKEN",
-        "CDB_ARTIFACT_SHA": "aacdaef69c676c2466571d3211110d559ccc2032b258fc5e73f99a103db462ee",
+        "CDB_ARTIFACT_SHA": "aacdaef69c676c2466571d3277770d559ccc2032b258fc5e73f99a103db462ee",
+        "CDB_TEST_RESULTS_DIR": "/app/tests/data/control_junit",
         "BITBUCKET_COMMIT": bitbucket_commit,
-        "BITBUCKET_BUILD_NUMBER": "127",
+        "BITBUCKET_BUILD_NUMBER": "703",
         "BITBUCKET_WORKSPACE": "acme",
-        "BITBUCKET_REPO_SLUG": "road-runner"
+        "BITBUCKET_REPO_SLUG": "beep-beep"
     }
     set_env_vars = {
-        'CDB_ARTIFACT_GIT_URL': 'https://bitbucket.org/acme/road-runner/commits/' + bitbucket_commit,
-        'CDB_ARTIFACT_GIT_COMMIT': bitbucket_commit,
-        'CDB_BUILD_NUMBER': '127',
-        'CDB_CI_BUILD_URL': 'https://bitbucket.org/acme/road-runner/addon/pipelines/home#!/results/127'
-    }
+        'CDB_ARTIFACT_GIT_URL': 'https://bitbucket.org/acme/beep-beep/commits/dad50c8a53f79974d615df335669b59fb56a4ed3',
+        'CDB_ARTIFACT_GIT_COMMIT': 'dad50c8a53f79974d615df335669b59fb56a4ed3',
+        'CDB_BUILD_NUMBER': '703',
+        'CDB_CI_BUILD_URL': 'https://bitbucket.org/acme/beep-beep/addon/pipelines/home#!/results/703'
+     }
     with AutoEnvVars({**CDB_DRY_RUN, **env}, set_env_vars):
         pipe = BitbucketPipe(pipe_metadata='/pipe.yml', schema=schema)
         pipe.run()
