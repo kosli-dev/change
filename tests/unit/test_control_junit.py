c6c58e6bfe77141b2c363f6bbaeb23fa35312cb5
Jon Jagger <jon@jaggersoft.com>
2021-01-27 10:04:31 +0000

    Collapse control_junit approval-test duplication

--- tests/unit/test_control_junit.py
+++ tests/unit/test_control_junit.py
@@ -69,7 +69,7 @@ def test_is_compliant_suite_returns_false_WHEN_errors_in_JUnitXML():
 
 
 def test_junit_parser_can_load_junit_output():
-    test_xml = load_test_results('tests/data/control_junit/TEST-Junit.xml')
+    test_xml = load_test_results(TEST_JUNIT_XML_FILENAME)
     # one test suite in file
     assert test_xml._tag == "testsuite"
     assert len(test_xml) == 2
@@ -78,13 +78,13 @@ def test_junit_parser_can_load_junit_output():
 
 
 def test_junit_parser_can_validate_junit_output():
-    (control_result, message) = is_compliant_test_results('tests/data/control_junit/TEST-Junit.xml')
+    (control_result, message) = is_compliant_test_results(TEST_JUNIT_XML_FILENAME)
     assert control_result is False
     assert message == "Tests contain failures"
 
 
 def test_junit_parser_can_load_pytest_output():
-    test_xml = load_test_results('tests/data/control_junit/TEST-Pytest-pass.xml')
+    test_xml = load_test_results(TEST_PYTEST_PASS_XML_FILENAME)
     # one test suite in file
     assert test_xml._tag == "testsuites"
     assert len(test_xml) == 1
@@ -93,13 +93,13 @@ def test_junit_parser_can_load_pytest_output():
 
 
 def test_junit_parser_can_validate_pytest_output():
-    (control_result, message) = is_compliant_test_results('tests/data/control_junit/TEST-Pytest-pass.xml')
+    (control_result, message) = is_compliant_test_results(TEST_PYTEST_PASS_XML_FILENAME)
     assert control_result is True
     assert message == "All tests passed"
 
 
 def test_junit_parser_can_load_pytest_failed_output():
-    test_xml = load_test_results('tests/data/control_junit/TEST-Pytest-fail.xml')
+    test_xml = load_test_results(TEST_PYTEST_FAIL_XML_FILENAME)
     # one test suite in file
     assert test_xml._tag == "testsuites"
     assert len(test_xml) == 1
@@ -109,13 +109,13 @@ def test_junit_parser_can_load_pytest_failed_output():
 
 
 def test_junit_parser_can_validate_pytest_failed_output():
-    (control_result, message) = is_compliant_test_results('tests/data/control_junit/TEST-Pytest-fail.xml')
+    (control_result, message) = is_compliant_test_results(TEST_PYTEST_FAIL_XML_FILENAME)
     assert control_result is False
     assert message == "Tests contain failures"
 
 
 def test_junit_parser_can_load_owasp_output():
-    test_xml = load_test_results('tests/data/control_junit/TEST-OWASP-pass.xml')
+    test_xml = load_test_results(TEST_OWASP_PASS_XML_FILENAME)
     # one test suite in file
     assert test_xml._tag == "testsuites"
     assert len(test_xml) == 5
@@ -125,13 +125,13 @@ def test_junit_parser_can_load_owasp_output():
 
 
 def test_junit_parser_can_validate_owasp_output():
-    (control_result, message) = is_compliant_test_results('tests/data/control_junit/TEST-OWASP-pass.xml')
+    (control_result, message) = is_compliant_test_results(TEST_OWASP_PASS_XML_FILENAME)
     assert control_result is True
     assert message == "All tests passed"
 
 
 def test_junit_parser_can_load_owasp_failed_output():
-    test_xml = load_test_results('tests/data/control_junit/TEST-OWASP-fail.xml')
+    test_xml = load_test_results(TEST_OWASP_FAIL_XML_FILENAME)
     # one test suite in file
     assert test_xml._tag == "testsuites"
     assert len(test_xml) == 88
@@ -141,13 +141,13 @@ def test_junit_parser_can_load_owasp_failed_output():
 
 
 def test_junit_parser_can_validate_owasp_failed_output():
-    (control_result, message) = is_compliant_test_results('tests/data/control_junit/TEST-OWASP-fail.xml')
+    (control_result, message) = is_compliant_test_results(TEST_OWASP_FAIL_XML_FILENAME)
     assert control_result is False
     assert message == "Tests contain failures"
 
 
 def test_junit_parser_can_load_surefire_output():
-    test_xml = load_test_results('tests/data/control_junit/surefire_examples/TEST-com.compliancedb.example.Example1Test.xml')
+    test_xml = load_test_results(surefire_examples_path('TEST-com.compliancedb.example.Example1Test.xml'))
     # one test suite in file
     assert test_xml._tag == "testsuite"
     # 7 testcases and one property
@@ -158,39 +158,40 @@ def test_junit_parser_can_load_surefire_output():
 
 
 def test_failing_surefire_testxml_results_in_non_compliant_evidence():
-    test_xml = load_test_results('tests/data/control_junit/surefire_examples/TEST-com.compliancedb.example.Example1Test.xml')
+    test_xml = load_test_results(surefire_examples_path('TEST-com.compliancedb.example.Example1Test.xml'))
     (control_result, message) = is_compliant_suite(test_xml)
     assert control_result is False
     assert message == "Tests contain failures"
 
 
 def test_passing_surefire_testxml_results_in_compliant_evidence():
-    test_xml = load_test_results('tests/data/control_junit/surefire_examples/TEST-com.compliancedb.example.Example2Test.xml')
+    test_xml = load_test_results(surefire_examples_path('TEST-com.compliancedb.example.Example2Test.xml'))
     (control_result, message) = is_compliant_suite(test_xml)
     assert control_result is True
     assert message == "All tests passed"
 
 
 def test_list_test_result_files():
-    files = ls_test_results('tests/data/control_junit/surefire_examples/')
-    assert files == ['tests/data/control_junit/surefire_examples/TEST-com.compliancedb.example.Example1Test.xml',
-                     'tests/data/control_junit/surefire_examples/TEST-com.compliancedb.example.Example2Test.xml']
+    files = ls_test_results(surefire_examples_path())
+    assert files == [surefire_examples_path('TEST-com.compliancedb.example.Example1Test.xml'),
+                     surefire_examples_path('TEST-com.compliancedb.example.Example2Test.xml')]
 
 
 def test_list_test_result_files_filters_failsafe_summary_xml():
-    files = ls_test_results('tests/data/control_junit/failsafe_examples/')
-    assert files == ['tests/data/control_junit/failsafe_examples/TEST-com.compliancedb.example.Example1Test.xml']
+    files = ls_test_results(failsafe_examples_path())
+    assert files == [failsafe_examples_path('TEST-com.compliancedb.example.Example1Test.xml')]
 
 
 @mock.patch('cdb.control_junit.is_compliant_test_results')
 @mock.patch('cdb.control_junit.ls_test_results')
 def test_is_compliant_tests_directory_passes_if_every_file_has_no_failures(mocked_ls, mocked_results):
     mocked_ls.return_value = \
-        ['tests/data/control_junit/surefire_examples/TEST-com.compliancedb.example.Example1Test.xml',
-         'tests/data/control_junit/surefire_examples/TEST-com.compliancedb.example.Example2Test.xml']
+        [surefire_examples_path('TEST-com.compliancedb.example.Example1Test.xml'),
+         surefire_examples_path('TEST-com.compliancedb.example.Example2Test.xml')]
 
     mocked_results.side_effect = [(True, "All good"), (True, "All good")]
-    (result, message) = is_compliant_tests_directory("tests/data/control_junit/surefire_examples")
+
+    (result, message) = is_compliant_tests_directory(surefire_examples_path())
 
     assert mocked_results.call_count == 2
     assert result
@@ -201,11 +202,11 @@ def test_is_compliant_tests_directory_passes_if_every_file_has_no_failures(mocke
 @mock.patch('cdb.control_junit.ls_test_results')
 def test_is_compliant_tests_directory_fails_if_a_file_has_failures(mocked_ls, mocked_results):
     mocked_ls.return_value = \
-        ['tests/data/control_junit/surefire_examples/TEST-com.compliancedb.example.Example1Test.xml',
-         'tests/data/control_junit/surefire_examples/TEST-com.compliancedb.example.Example2Test.xml']
+        [surefire_examples_path('TEST-com.compliancedb.example.Example1Test.xml'),
+         surefire_examples_path('TEST-com.compliancedb.example.Example2Test.xml')]
 
     mocked_results.side_effect = [(True, "All good"), (False, "Bad stuff")]
-    (result, message) = is_compliant_tests_directory("tests/data/control_junit/surefire_examples")
+    (result, message) = is_compliant_tests_directory(surefire_examples_path())
 
     assert mocked_results.call_count == 2
     assert result is False
@@ -213,7 +214,41 @@ def test_is_compliant_tests_directory_fails_if_a_file_has_failures(mocked_ls, mo
 
 
 def test_passing_failsafe_testxml_results_in_compliant_evidence():
-    test_xml = load_test_results('tests/data/control_junit/failsafe_examples/TEST-com.compliancedb.example.Example1Test.xml')
+    test_xml = load_test_results(failsafe_examples_path('TEST-com.compliancedb.example.Example1Test.xml'))
     (control_result, message) = is_compliant_suite(test_xml)
     assert control_result is True
     assert message == "All tests passed"
+
+
+# Helpers
+
+CONTROL_JUNIT_TEST_DATA_ROOT = 'tests/data/control_junit'
+
+
+def control_junit_data_path(filename):
+    return CONTROL_JUNIT_TEST_DATA_ROOT + '/' + filename
+
+
+TEST_JUNIT_XML_FILENAME = control_junit_data_path('TEST-Junit.xml')
+
+TEST_PYTEST_PASS_XML_FILENAME = control_junit_data_path('TEST-Pytest-pass.xml')
+TEST_PYTEST_FAIL_XML_FILENAME = control_junit_data_path('TEST-Pytest-fail.xml')
+
+TEST_OWASP_PASS_XML_FILENAME = control_junit_data_path('TEST-OWASP-pass.xml')
+TEST_OWASP_FAIL_XML_FILENAME = control_junit_data_path('TEST-OWASP-fail.xml')
+
+
+def surefire_examples_path(filename=None):
+    dir_name = CONTROL_JUNIT_TEST_DATA_ROOT + '/surefire_examples/'
+    if filename is None:
+        return dir_name
+    else:
+        return dir_name + filename
+
+
+def failsafe_examples_path(filename=None):
+    dir_name = CONTROL_JUNIT_TEST_DATA_ROOT + '/failsafe_examples/'
+    if filename is None:
+        return dir_name
+    else:
+        return dir_name + filename
