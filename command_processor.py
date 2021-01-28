9f17d7cc4b54af2e196eee0844b0747ec5406b96
Mike Long <mike@compliancedb.com>
2021-01-28 15:39:11 +0100

    Change production url to app.compliancedb.com

--- command_processor.py
+++ command_processor.py
@@ -12,7 +12,7 @@ def execute(env):
     merkleypipe_path = "/Merkelypipe.json"
     with open(merkleypipe_path) as merkelypipe_file:
         merkelypipe_data = load_merkelypipe(merkelypipe_file)
-        host = "https://app.merkely.com"
+        host = "https://app.compliancedb.com"
         pipelines_url = ApiSchema.url_for_pipelines(host, merkelypipe_data)
         api_token = env.get('MERKELY_API_TOKEN', None)
         http_put_payload(url=pipelines_url, payload=merkelypipe_data, api_token=api_token)
