b01cd071e31f556540f3cda2a05ea8fd7e779a96
Jon Jagger <jon@jaggersoft.com>
2021-01-30 19:30:36 +0000

    Add tests for ScopedDirCopier()

---
+++ tests/unit/test_scoped_dir_copier.py
@@ -0,0 +1,47 @@
+from os import path
+from tests.utils import ScopedDirCopier
+
+from pytest import raises
+
+
+def test_target_dir_exists_in_with_statement_but_not_before_or_after():
+    source_dir = '/app/tests/data/control_junit'
+    target_dir = '/tmp/a/b/c'
+
+    assert path.isdir(source_dir)
+    assert not path.isdir(target_dir)
+
+    with ScopedDirCopier(source_dir, target_dir):
+        assert path.isdir(target_dir)
+
+    assert not path.isdir(target_dir)
+
+
+def test_source_dir_does_not_exist_raises():
+    source_dir = '/app/tests/data/does/not/exist'
+    target_dir = '/tmp/a/b/c'
+
+    assert not path.isdir(source_dir)
+    assert not path.isdir(target_dir)
+
+    with raises(ScopedDirCopier.Error) as exc:
+        with ScopedDirCopier(source_dir, target_dir):
+            pass
+
+    assert not path.isdir(source_dir)
+    assert not path.isdir(target_dir)
+
+
+def test_target_dir_already_exists_raises():
+    source_dir = '/app/tests/data'
+    target_dir = '/app/tests/data'
+
+    assert path.isdir(source_dir)
+    assert path.isdir(target_dir)
+
+    with raises(ScopedDirCopier.Error) as exc:
+        with ScopedDirCopier(source_dir, target_dir):
+            pass
+
+    assert path.isdir(source_dir)
+    assert path.isdir(target_dir)
--- tests/utils/scoped_dir_copier.py
+++ tests/utils/scoped_dir_copier.py
@@ -1,19 +1,21 @@
-from os import mkdir, path
+from os import path
 from distutils.dir_util import copy_tree, remove_tree
 
 
 class ScopedDirCopier(object):
     def __init__(self, source_dir, target_dir):
         if not path.isdir(source_dir):
-            raise ValueError("source dir '{} does not exist".format(source_dir))
+            raise self.Error("source dir '{} does not exist".format(source_dir))
         if path.exists(target_dir):
-            raise ValueError("target dir '{}' already exists".format(target_dir))
+            raise self.Error("target dir '{}' already exists".format(target_dir))
         self._source_dir = source_dir
         self._target_dir = target_dir
 
     def __enter__(self):
-        mkdir(self._target_dir)
         copy_tree(self._source_dir, self._target_dir)
 
     def __exit__(self, _type, _value, _traceback):
         remove_tree(self._target_dir)
+
+    class Error(ValueError):
+        pass
