d0394b5d63d14a6d1161cf9dbfe86e52b78ee10a
Jon Jagger <jon@jaggersoft.com>
2021-01-30 19:47:22 +0000

    Add test for MERKELY_COMMAND=log_artifact with MERKELY_FINGERPRINT=file://... not at root dir

--- tests/unit/test_m_log_artifact.py
+++ tests/unit/test_m_log_artifact.py
@@ -7,7 +7,7 @@ def test_command_processor_log_artifact_file(capsys):
     ev = {
         "MERKELY_COMMAND": "log_artifact",
         "MERKELY_API_TOKEN": "MY_SUPER_SECRET_API_TOKEN",
-        "MERKELY_FINGERPRINT": "file://coverage.txt",
+        "MERKELY_FINGERPRINT": "file://coverage.txt",  # at root
         "MERKELY_CI_BUILD_URL": "https://gitlab/build/1456",
         "MERKELY_CI_BUILD_NUMBER": "23",
         "MERKELY_ARTIFACT_GIT_URL": "http://github/me/project/commit/" + commit,
@@ -23,6 +23,31 @@ def test_command_processor_log_artifact_file(capsys):
                     'sha_digest_for_file': lambda _filename: "ccdd89ccdc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
                 }
                 status_code = command_processor.execute(context)
+
+    assert status_code == 0
+    verify_approval(capsys)
+
+
+def test_command_processor_log_artifact_file_not_at_root(capsys):
+    commit = "abc50c8a53f79974d615df335669b59fb56a4444"
+    ev = {
+        "MERKELY_COMMAND": "log_artifact",
+        "MERKELY_API_TOKEN": "MY_SUPER_SECRET_API_TOKEN",
+        "MERKELY_FINGERPRINT": "file://app/tests/data/coverage.txt",  # not at root
+        "MERKELY_CI_BUILD_URL": "https://gitlab/build/1456",
+        "MERKELY_CI_BUILD_NUMBER": "24",
+        "MERKELY_ARTIFACT_GIT_URL": "http://github/me/project/commit/" + commit,
+        "MERKELY_ARTIFACT_GIT_COMMIT": commit,
+        "MERKELY_IS_COMPLIANT": "FALSE"
+    }
+
+    with ScopedEnvVars({**CDB_DRY_RUN, **ev}) as env:
+        with ScopedFileCopier("/app/tests/data/Merkelypipe.json", "/Merkelypipe.json"):
+            context = {
+                'env': env,
+                'sha_digest_for_file': lambda _filename: "ccdd89ccdc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f5115"
+            }
+            status_code = command_processor.execute(context)
 
     assert status_code == 0
     verify_approval(capsys)
