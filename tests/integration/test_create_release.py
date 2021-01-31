39ddd79334ce89ed315fc3073e4df1b67547da0f
Arstan <arstanrys@gmail.com>
2021-01-31 21:01:42 +0100

    Fix approval-test for create_release()

--- tests/integration/test_create_release.py
+++ tests/integration/test_create_release.py
@@ -4,15 +4,20 @@ from tests.utils import ScopedEnvVars, ScopedDirCopier, CDB_DRY_RUN, verify_appr
 from tests.unit.test_git import TEST_REPO_ROOT
 
 
-def test_required_env_vars_and_CDB_ARTIFACT_SHA_is_none(capsys):
+def test_required_env_vars_and_CDB_ARTIFACT_SHA_is_none(capsys, mocker):
     env = {
         "CDB_API_TOKEN": "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
-        "CDB_ARTIFACT_SHA": "None",
         "CDB_BASE_SRC_COMMITISH": "production",
         "CDB_TARGET_SRC_COMMITISH": "master",
     }
+    mock_artifacts_for_commit = {
+        "artifacts": [{"sha256": "bbcdaef69c676c2466571d3233380d559ccc2032b258fc5e73f99a103db46212"}]
+    }
     set_env_vars = {}
+
     with ScopedEnvVars({**CDB_DRY_RUN, **env}, set_env_vars), ScopedDirCopier("/test_src", "/src"):
+        mocker.patch('cdb.create_release.get_artifact_sha', return_value=None)
+        mocker.patch('cdb.create_release.get_artifacts_for_commit', return_value=mock_artifacts_for_commit)
         create_release("tests/integration/test-pipefile.json")
     verify_approval(capsys, ["out"])
 
