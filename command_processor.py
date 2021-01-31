c093cce114894031030196972f1eb7dc79ebc1e1
Jon Jagger <jon@jaggersoft.com>
2021-01-31 08:53:36 +0000

    Refactor: Add LogArtifactCommand() class

--- command_processor.py
+++ command_processor.py
@@ -1,28 +1,21 @@
 from cdb.api_schema import ApiSchema
 from cdb.http import http_put_payload
+import json
 
 
 MERKELYPIPE_PATH = "/Merkelypipe.json"
 
 def execute(context):
-    def env(name):
-        return get_env(context, name)
-    command = env("MERKELY_COMMAND")
-    with open(MERKELYPIPE_PATH) as file:
-        merkelypipe = load_merkelypipe(file)
-        api_token = env('MERKELY_API_TOKEN')
-        host = env("MERKELY_HOST")
-        if command == "declare_pipeline":
-            #declare_pipeline(context, merkelypipe, api_token, host)
-            DeclarePipelineCommand(context).execute()
-        if command == "log_artifact":
-            print("MERKELY_COMMAND={}".format(command))
-            log_artifact(context, merkelypipe, api_token, host)
-
+    command = context['env'].get("MERKELY_COMMAND", None)
+    if command == "declare_pipeline":
+        DeclarePipelineCommand(context).execute()
+    if command == "log_artifact":
+        LogArtifactCommand(context).execute()
     return 0
 
 
 class Command:
+
     def __init__(self, context):
         self._context = context
 
@@ -53,79 +46,61 @@ class Command:
 
 class DeclarePipelineCommand(Command):
 
-    def __init__(self, context):
-        super().__init__(context)
-
     def concrete_execute(self):
         pipelines_url = ApiSchema.url_for_pipelines(self.host, self.merkelypipe)
         http_put_payload(url=pipelines_url, payload=self.merkelypipe, api_token=self.api_token)
 
 
+class LogArtifactCommand(Command):
 
+    def concrete_execute(self):
+        fingerprint = self._env("MERKELY_FINGERPRINT")
+        context = self._context
+        context['description'] = "Created by build " + self._env('MERKELY_CI_BUILD_NUMBER')
+        context['git_commit'] = self._env('MERKELY_ARTIFACT_GIT_COMMIT')
+        context['commit_url'] = self._env('MERKELY_ARTIFACT_GIT_URL')
+        context['build_url'] = self._env('MERKELY_CI_BUILD_URL')
+        context['is_compliant'] = self._env('MERKELY_IS_COMPLIANT') == "TRUE"
 
-FILE_PROTOCOL = "file://"
-DOCKER_PROTOCOL = "docker://"
+        FILE_PROTOCOL = "file://"
+        if fingerprint.startswith(FILE_PROTOCOL):
+            self._log_artifact_file(fingerprint[len(FILE_PROTOCOL):])
 
-def log_artifact(context, merkelypipe, api_token, host):
-    def env(name):
-        return get_env(context, name)
+        DOCKER_PROTOCOL = "docker://"
+        if fingerprint.startswith(DOCKER_PROTOCOL):
+            self._log_artifact_docker_image(fingerprint[len(DOCKER_PROTOCOL):])
 
-    fingerprint = env("MERKELY_FINGERPRINT")
+    def _log_artifact_file(self, filename):
+        context = self._context
+        pathed_filename = '/' + filename
+        print("Getting SHA for file:// artifact: " + pathed_filename)
+        artifact_sha = context['sha_digest_for_file'](pathed_filename)
+        print("Calculated digest: " + artifact_sha)
+        # print("Publish artifact to ComplianceDB")
+        print('MERKELY_IS_COMPLIANT: ' + str(context['is_compliant']))
+        create_artifact(self.api_token, self.host, self.merkelypipe,
+                        artifact_sha, pathed_filename,
+                        context['description'],
+                        context['git_commit'],
+                        context['commit_url'],
+                        context['build_url'],
+                        context['is_compliant'])
 
-    context['description'] = "Created by build " + env('MERKELY_CI_BUILD_NUMBER')
-    context['git_commit'] = env('MERKELY_ARTIFACT_GIT_COMMIT')
-    context['commit_url'] = env('MERKELY_ARTIFACT_GIT_URL')
-    context['build_url'] = env('MERKELY_CI_BUILD_URL')
-    context['is_compliant'] = env('MERKELY_IS_COMPLIANT') == "TRUE"
+    def _log_artifact_docker_image(self, image_name):
+        context = self._context
+        print("Getting SHA for docker:// artifact: " + image_name)
+        artifact_sha = context['sha_digest_for_docker_image'](image_name)
+        print("Calculated digest: " + artifact_sha)
+        # print("Publish artifact to ComplianceDB")
+        print('MERKELY_IS_COMPLIANT: ' + str(context['is_compliant']))
+        create_artifact(self.api_token, self.host, self.merkelypipe,
+                        artifact_sha, image_name,
+                        context['description'],
+                        context['git_commit'],
+                        context['commit_url'],
+                        context['build_url'],
+                        context['is_compliant'])
 
-    if fingerprint.startswith(FILE_PROTOCOL):
-        context['artifact_name'] = fingerprint[len(FILE_PROTOCOL):]
-        log_artifact_file(context, merkelypipe, api_token, host)
-
-    if fingerprint.startswith(DOCKER_PROTOCOL):
-        context['artifact_name'] = fingerprint[len(DOCKER_PROTOCOL):]
-        log_artifact_docker_image(context, merkelypipe, api_token, host)
-
-
-def log_artifact_file(context, merkelypipe, api_token, host):
-    pathed_filename = '/' + context['artifact_name']
-    print("Getting SHA for file:// artifact: " + pathed_filename)
-    artifact_sha = context['sha_digest_for_file'](pathed_filename)
-    print("Calculated digest: " + artifact_sha)
-    # print("Publish artifact to ComplianceDB")
-    print('MERKELY_IS_COMPLIANT: ' + str(context['is_compliant']))
-    create_artifact(api_token, host, merkelypipe,
-                    artifact_sha, pathed_filename,
-                    context['description'],
-                    context['git_commit'],
-                    context['commit_url'],
-                    context['build_url'],
-                    context['is_compliant'])
-
-
-def log_artifact_docker_image(context, merkelypipe, api_token, host):
-    image_name = context['artifact_name']
-    print("Getting SHA for docker:// artifact: " + image_name)
-    artifact_sha = context['sha_digest_for_docker_image'](image_name)
-    print("Calculated digest: " + artifact_sha)
-    # print("Publish artifact to ComplianceDB")
-    print('MERKELY_IS_COMPLIANT: ' + str(context['is_compliant']))
-    create_artifact(api_token, host, merkelypipe,
-                    artifact_sha, image_name,
-                    context['description'],
-                    context['git_commit'],
-                    context['commit_url'],
-                    context['build_url'],
-                    context['is_compliant'])
-
-
-def get_env(context, name):
-    return context['env'].get(name, None)
-
-
-import json
-def load_merkelypipe(file):
-    return json.load(file)
 
 
 def create_artifact(api_token, host, merkelypipe,
