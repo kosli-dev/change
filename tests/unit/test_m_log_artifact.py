09209349ba4311a5b141253ff64edfe9e0da07da
Jon Jagger <jon@jaggersoft.com>
2021-02-01 16:14:02 +0000

    Refactor: collapse log_artifact test duplication

--- tests/unit/test_m_log_artifact.py
+++ tests/unit/test_m_log_artifact.py
@@ -5,17 +5,8 @@ from tests.utils import *
 def test_command_processor_log_artifact_file(capsys):
     commit = "abc50c8a53f79974d615df335669b59fb56a4ed3"
     digest = "ccdd89ccdc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
-    ev = {
-        "MERKELY_COMMAND": "log_artifact",
-        "MERKELY_API_TOKEN": "MY_SUPER_SECRET_API_TOKEN",
-        "MERKELY_FINGERPRINT": "file://coverage.txt",  # at root
-        "MERKELY_HOST": "https://test.merkely.com",
-        "MERKELY_CI_BUILD_URL": "https://gitlab/build/1456",
-        "MERKELY_CI_BUILD_NUMBER": "23",
-        "MERKELY_ARTIFACT_GIT_URL": "http://github/me/project/commit/" + commit,
-        "MERKELY_ARTIFACT_GIT_COMMIT": commit,
-        "MERKELY_IS_COMPLIANT": "TRUE"
-    }
+    ev = log_artifact_env(commit)
+    ev["MERKELY_FINGERPRINT"] = "file://coverage.txt"  # at root
 
     with ScopedEnvVars({**DRY_RUN, **ev}) as env:
         with ScopedFileCopier("/app/tests/data/coverage.txt", "/coverage.txt"):
@@ -31,17 +22,8 @@ def test_command_processor_log_artifact_file(capsys):
 def test_command_processor_log_artifact_file_not_at_root(capsys):
     commit = "abc50c8a53f79974d615df335669b59fb56a4444"
     digest = "ccdd89ccdc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f5115"
-    ev = {
-        "MERKELY_COMMAND": "log_artifact",
-        "MERKELY_API_TOKEN": "MY_SUPER_SECRET_API_TOKEN",
-        "MERKELY_HOST": "https://test.merkely.com",
-        "MERKELY_FINGERPRINT": "file://app/tests/data/coverage.txt",  # not at root
-        "MERKELY_CI_BUILD_URL": "https://gitlab/build/1456",
-        "MERKELY_CI_BUILD_NUMBER": "24",
-        "MERKELY_ARTIFACT_GIT_URL": "http://github/me/project/commit/" + commit,
-        "MERKELY_ARTIFACT_GIT_COMMIT": commit,
-        "MERKELY_IS_COMPLIANT": "FALSE"
-    }
+    ev = log_artifact_env(commit)
+    ev["MERKELY_FINGERPRINT"] = "file://app/tests/data/coverage.txt"  # not at root
 
     with ScopedEnvVars({**DRY_RUN, **ev}) as env:
         with ScopedFileCopier("/app/tests/data/Merkelypipe.json", "/Merkelypipe.json"):
@@ -56,17 +38,8 @@ def test_command_processor_log_artifact_file_not_at_root(capsys):
 def test_command_processor_log_artifact_docker(capsys):
     commit = "ddc50c8a53f79974d615df335669b59fb56a4ed3"
     digest = "ddee5566dc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
-    ev = {
-        "MERKELY_COMMAND": "log_artifact",
-        "MERKELY_API_TOKEN": "MY_SUPER_SECRET_API_TOKEN",
-        "MERKELY_HOST": "https://test.merkely.com",
-        "MERKELY_FINGERPRINT": "docker://acme/road-runner:2.3",
-        "MERKELY_CI_BUILD_URL": "https://gitlab/build/1856",
-        "MERKELY_CI_BUILD_NUMBER": "236",
-        "MERKELY_ARTIFACT_GIT_URL": "http://github/me/project/commit/" + commit,
-        "MERKELY_ARTIFACT_GIT_COMMIT": commit,
-        "MERKELY_IS_COMPLIANT": "TRUE"
-    }
+    ev = log_artifact_env(commit)
+    ev["MERKELY_FINGERPRINT"] = "docker://acme/road-runner:6.8"
 
     with ScopedEnvVars({**DRY_RUN, **ev}) as env:
         with ScopedFileCopier("/app/tests/data/Merkelypipe.json", "/Merkelypipe.json"):
@@ -102,3 +75,16 @@ def X_test_command_processor_log_artifact_sha256(capsys):
     assert status_code == 0
     verify_approval(capsys)
 
+
+def log_artifact_env(commit):
+    return {
+        "MERKELY_COMMAND": "log_artifact",
+        "MERKELY_API_TOKEN": "MY_SUPER_SECRET_API_TOKEN",
+        "MERKELY_FINGERPRINT": "file://coverage.txt",  # at root
+        "MERKELY_HOST": "https://test.merkely.com",
+        "MERKELY_CI_BUILD_URL": "https://gitlab/build/1456",
+        "MERKELY_CI_BUILD_NUMBER": "23",
+        "MERKELY_ARTIFACT_GIT_URL": "http://github/me/project/commit/" + commit,
+        "MERKELY_ARTIFACT_GIT_COMMIT": commit,
+        "MERKELY_IS_COMPLIANT": "TRUE"
+    }
