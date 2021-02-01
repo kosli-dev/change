e22ac9a20402bd5a95618b0e6fb723fbf45cc4f0
Jon Jagger <jon@jaggersoft.com>
2021-02-01 12:17:00 +0000

    Add test for MERKELY_COMMAND set to empty string

--- commands/command_processor.py
+++ commands/command_processor.py
@@ -5,9 +5,7 @@ from commands import LogArtifactCommand
 
 def execute(context):
     try:
-        command = context.env.get("MERKELY_COMMAND", None)
-        if command is None:
-            raise CommandError(23, "MERKELY_COMMAND environment-variable not set")
+        command = get_command(context)
         if command == "declare_pipeline":
             DeclarePipelineCommand(context).execute()
         if command == "log_artifact":
@@ -18,4 +16,10 @@ def execute(context):
         return exc.status_code
 
 
-
+def get_command(context):
+    command = context.env.get("MERKELY_COMMAND", None)
+    if command is None:
+        raise CommandError(23, "MERKELY_COMMAND environment-variable not set")
+    if command == "":
+        raise CommandError(24, "MERKELY_COMMAND environment-variable is empty string")
+    return command
--- tests/unit/test_m_command_processor.py
+++ tests/unit/test_m_command_processor.py
@@ -13,7 +13,21 @@ def test_raises_when_merkely_command_not_set(capsys):
     with ScopedEnvVars({**CDB_DRY_RUN, **ev}) as env:
         status_code = command_processor.execute(make_context(env))
 
-    assert status_code == 23
+    assert status_code != 0
+    verify_approval(capsys)
+
+
+def test_raises_when_merkely_command_is_empty_string(capsys):
+    ev = {
+        "MERKELY_COMMAND": "",
+        "MERKELY_API_TOKEN": "MY_SUPER_SECRET_API_TOKEN",
+        "MERKELY_HOST": "https://test.merkely.com"
+    }
+
+    with ScopedEnvVars({**CDB_DRY_RUN, **ev}) as env:
+        status_code = command_processor.execute(make_context(env))
+
+    assert status_code != 0
     verify_approval(capsys)
 
 
