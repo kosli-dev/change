6e0648f83e41f2331840575d6db905375edd6726
Jon Jagger <jon@jaggersoft.com>
2021-02-03 19:59:26 +0000

    Add test for MERKELY_COMMAND=log_deployment FINGERPRINT=docker://...

--- tests/unit/test_m_log_deployment.py
+++ tests/unit/test_m_log_deployment.py
@@ -1,6 +1,9 @@
 from commands import command_processor
 from tests.utils import *
 
+# def test_file_not_at_root(capsys):
+
+
 def test_file_at_root(capsys):
     commit = "abc50c8a53f79974d615df335669b59fb56a4ed3"
     digest = "ccdd89ccdc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
@@ -14,12 +17,26 @@ def test_file_at_root(capsys):
             status_code = command_processor.execute(context)
 
     assert status_code == 0
-    #verify_approval(capsys)
-    verify_payload_and_url(capsys)
+    verify_approval(capsys)
+    #verify_payload_and_url(capsys)
 
 
-#def test_file_not_at_root(capsys):
-#def test_docker_image(capsys):
+def test_docker_image(capsys):
+    commit = "abc50c8a53f79974d615df335669b59fb56a4ed3"
+    digest = "ccdd89ccdc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
+    ev = log_deployment_env(commit)
+    ev["MERKELY_FINGERPRINT"] = "docker://acme/road-runner:6.8"
+
+    with dry_run(ev) as env, scoped_merkelypipe_json():
+        with ScopedFileCopier("/app/tests/data/coverage.txt", "/jam.jar"):
+            context = make_context(env)
+            context.sha_digest_for_docker_image = lambda _filename: digest
+            status_code = command_processor.execute(context)
+
+    assert status_code == 0
+    verify_approval(capsys)
+    #verify_payload_and_url(capsys)
+
 #def test_sha256_file(capsys):
 #def test_sha256_docker_image(capsys):
 
