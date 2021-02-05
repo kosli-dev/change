95f1c5a3d33abc4db65ad2d6c0dbfd1bab6cd53b
Jon Jagger <jon@jaggersoft.com>
2021-02-05 08:11:55 +0000

    Refactor

--- tests/unit/test_m_log_artifact.py
+++ tests/unit/test_m_log_artifact.py
@@ -33,21 +33,25 @@ def test_file_at_root(capsys, mocker):
     build_url_number = '1456'
     build_number = '349'
 
-    old_env = {
-        "CDB_API_TOKEN": API_TOKEN,
-        "CDB_ARTIFACT_FILENAME": filename,
-        "CDB_IS_COMPLIANT": "TRUE",
-        "CDB_ARTIFACT_GIT_COMMIT": commit,
-        "CDB_ARTIFACT_GIT_URL": f"https://github/me/project/commit/{commit}",
-        "CDB_CI_BUILD_URL": f"https://gitlab/build/{build_url_number}",
-        "CDB_BUILD_NUMBER": build_number
-    }
+    # make cdb call
+    old_env = old_put_artifact_env(commit,
+                                   build_url_number=build_url_number,
+                                   build_number=build_number)
+    old_env["CDB_ARTIFACT_FILENAME"] = filename
     set_env_vars = {'CDB_ARTIFACT_SHA': sha256}
-
     with ScopedEnvVars({**CDB_DRY_RUN, **old_env}, set_env_vars):
         mocker.patch('cdb.cdb_utils.calculate_sha_digest_for_file', return_value=sha256)
         put_artifact("tests/integration/test-pipefile.json")
+
+    # compare with approved cdb text file
     verify_approval(capsys, ["out"])
+
+    # extract data from approved cdb text file
+    this_test = "test_file_at_root"
+    approved = f"{APPROVAL_DIR}/{APPROVAL_FILE}.{this_test}.approved.txt"
+    with open(approved) as file:
+        old_approval = file.read()
+    _old_blurb, old_method, old_payload, old_url = blurb_method_payload_url(old_approval)
 
     expected_method = "Putting"
     expected_url = f"https://{domain}/api/v1/projects/{owner}/{name}/artifacts/"
@@ -61,17 +65,16 @@ def test_file_at_root(capsys, mocker):
         'sha256': sha256,
     }
 
-    this_test = "test_file_at_root"
-    approved = f"{APPROVAL_DIR}/{APPROVAL_FILE}.{this_test}.approved.txt"
-    with open(approved) as file:
-        old_approval = file.read()
-    _old_blurb, old_method, old_payload, old_url = blurb_method_payload_url(old_approval)
-
+    # verify data from approved cdb text file
     assert old_method == expected_method
     assert old_url == expected_url
     assert old_payload == expected_payload
 
-    ev = new_log_artifact_env(commit, domain, build_url_number, build_number)
+    # make merkely call
+    ev = new_log_artifact_env(commit,
+                              domain=domain,
+                              build_url_number=build_url_number,
+                              build_number=build_number)
     ev["MERKELY_FINGERPRINT"] = f"{protocol}{directory}{filename}"
     merkelypipe = "Merkelypipe.compliancedb.json"
     with dry_run(ev) as env, scoped_merkelypipe_json(merkelypipe):
@@ -80,6 +83,7 @@ def test_file_at_root(capsys, mocker):
             context.sha_digest_for_file = lambda _filename: sha256
             method, url, payload = command_processor.execute(context)
 
+    # verify matching data
     assert method == expected_method
     assert url == expected_url
     assert payload == expected_payload
@@ -90,7 +94,6 @@ def test_file_at_root(capsys, mocker):
         f"Calculated digest: {sha256}",
         'MERKELY_IS_COMPLIANT: True'
     ]
-
 
 
 def test_file_not_at_root(capsys):
@@ -176,7 +179,10 @@ def test_sha256_file(capsys):
     name = CDB_NAME
     build_url_number = '2156'
     build_number = '751'
-    ev = new_log_artifact_env(commit, domain, build_url_number, build_number)
+    ev = new_log_artifact_env(commit,
+                              domain=domain,
+                              build_url_number=build_url_number,
+                              build_number=build_number)
     ev["MERKELY_FINGERPRINT"] = f"{protocol}{sha256}"
     ev["MERKELY_DISPLAY_NAME"] = filename
 
@@ -263,7 +269,23 @@ def make_command_args():
     return make_command(context).args
 
 
-def new_log_artifact_env(commit, domain=None, build_url_number=None, build_number=None):
+def old_put_artifact_env(commit, *,
+                         build_url_number,
+                         build_number):
+    return {
+        "CDB_API_TOKEN": API_TOKEN,
+        "CDB_IS_COMPLIANT": "TRUE",
+        "CDB_ARTIFACT_GIT_COMMIT": commit,
+        "CDB_ARTIFACT_GIT_URL": f"https://github/me/project/commit/{commit}",
+        "CDB_CI_BUILD_URL": f"https://gitlab/build/{build_url_number}",
+        "CDB_BUILD_NUMBER": build_number
+    }
+
+
+def new_log_artifact_env(commit, *,
+                         domain=None,
+                         build_url_number=None,
+                         build_number=None):
     if domain is None:
         domain = "app.compliancedb.com"
     if build_url_number is None:
@@ -274,7 +296,7 @@ def new_log_artifact_env(commit, domain=None, build_url_number=None, build_numbe
         "MERKELY_COMMAND": "log_artifact",
         "MERKELY_API_TOKEN": API_TOKEN,
         "MERKELY_HOST": f"https://{domain}",
-        "MERKELY_FINGERPRINT": "file://jam.jar",
+        "MERKELY_FINGERPRINT": 'dummy',
         "MERKELY_CI_BUILD_URL": f"https://gitlab/build/{build_url_number}",
         "MERKELY_CI_BUILD_NUMBER": build_number,
         "MERKELY_ARTIFACT_GIT_URL": "https://github/me/project/commit/" + commit,
