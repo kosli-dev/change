7a44e63b59673f35ad6e54d23d1fc2187ed4045a
Jon Jagger <jon@jaggersoft.com>
2021-02-05 14:54:10 +0000

    Prepare contract test

--- tests/unit/test_m_log_artifact.py
+++ tests/unit/test_m_log_artifact.py
@@ -1,5 +1,8 @@
-from cdb.put_artifact import put_artifact
+from cdb.put_artifact       import put_artifact
+from cdb.put_artifact_image import put_artifact_image
+
 from commands import command_processor, make_command, RequiredEnvVar
+
 from tests.utils import *
 
 MERKELY_DOMAIN = "test.compliancedb.com"
@@ -15,7 +18,7 @@ API_TOKEN = "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4"
 
 
 def test_file_protocol_at_root(capsys, mocker):
-    # Data
+    # input data
     commit = "abc50c8a53f79974d615df335669b59fb56a4ed3"
     protocol = "file://"
     directory = ""
@@ -132,7 +135,8 @@ def test_file_protocol_not_at_root(capsys):
     ]
 
 
-def test_docker_protocol_image(capsys):
+def test_docker_protocol_image(capsys, mocker):
+    # input data
     commit = "ddc50c8a53f79974d615df335669b59fb56a4ed3"
     sha256 = "ddee5566dc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
     protocol = "docker://"
@@ -143,6 +147,7 @@ def test_docker_protocol_image(capsys):
     ev = new_log_artifact_env(commit)
     ev["MERKELY_FINGERPRINT"] = f"{protocol}{image_name}"
 
+    # make merkely call
     merkelypipe = "Merkelypipe.compliancedb.json"
     with dry_run(ev) as env, scoped_merkelypipe_json(merkelypipe):
         context = make_context(env)
@@ -166,9 +171,56 @@ def test_docker_protocol_image(capsys):
         f"Calculated digest: {sha256}",
         'MERKELY_IS_COMPLIANT: True'
     ]
-    #old_dir = "tests/integration/approved_executions"
-    #old_file = "test_put_artifact_image"
-    #old_test = "test_required_env_vars_uses_CDB_ARTIFACT_DOCKER_IMAGE"
+
+    # make cdb call
+    sha256 = "ddcdaef69c676c2466571d3233380d559ccc2032b258fc5e73f99a103db462ee"
+    commit = "12037940e4e7503055d8a8eea87e177f04f14616"
+    artifact_name = "acme/widget:3.4"
+
+    old_env = {
+        "CDB_API_TOKEN": "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
+        "CDB_ARTIFACT_DOCKER_IMAGE": artifact_name,
+        "CDB_IS_COMPLIANT": "TRUE",
+        "CDB_ARTIFACT_GIT_URL": f"http://github/me/project/commit/{commit}",
+        "CDB_ARTIFACT_GIT_COMMIT": commit,
+    }
+    set_env_vars = {}
+
+    with ScopedEnvVars({**CDB_DRY_RUN, **old_env}, set_env_vars):
+        mocker.patch('cdb.cdb_utils.calculate_sha_digest_for_docker_image', return_value=sha256)
+        put_artifact_image("tests/integration/test-pipefile.json")
+
+    # compare with approved cdb text file
+    verify_approval(capsys, ["out"])
+
+    # extract data from approved cdb text file
+    this_test = "test_docker_protocol_image"
+    approved = f"{APPROVAL_DIR}/{APPROVAL_FILE}.{this_test}.approved.txt"
+    with open(approved) as file:
+        old_approval = file.read()
+    _old_blurb, old_method, old_payload, old_url = extract_blurb_method_payload_url(old_approval)
+
+    domain = "app.compliancedb.com"
+    owner = "compliancedb"
+    name = "cdb-controls-test-pipeline"
+
+    expected_method = "Putting"
+    expected_url = f"https://{domain}/api/v1/projects/{owner}/{name}/artifacts/"
+    expected_payload = {
+        "build_url": "BUILD_URL_UNDEFINED",
+        "commit_url": f"http://github/me/project/commit/{commit}",
+        "description": "Created by build UNDEFINED",
+        "filename": artifact_name,
+        "git_commit": commit,
+        "is_compliant": True,
+        "sha256": sha256
+    }
+
+    # verify data from approved cdb text file
+    assert old_method == expected_method
+    assert old_url == expected_url
+    assert old_payload == expected_payload
+
 
 
 def test_sha256_protocol_file(capsys):
