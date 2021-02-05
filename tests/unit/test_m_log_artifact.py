6130c8ab4b55089584a31fcd06d38aebe511362d
Jon Jagger <jon@jaggersoft.com>
2021-02-05 13:57:47 +0000

    Restore prematurely deleted test

--- tests/unit/test_m_log_artifact.py
+++ tests/unit/test_m_log_artifact.py
@@ -268,9 +268,9 @@ def test_sha256_protocol_docker_image(capsys):
 # TODO: Test when docker image not found (decent error message)
 # TODO: Test when image not pushed (eg to dockerhub) so cannot get digest (decent error message)
 # TODO: Test when docker socket not volume-mounted
-# TODO: Test when sha256://SHA and SHA does not look like a SHA
+# TODO: Test when sha256://SHA and SHA does not look like a SHA?
 # TODO: test_sha256_file() when DISPLAY_NAME is missing
-# TODO: test_sha256_file() when supplied DISPLAY_NAME has full path...
+# TODO: test_sha256_file() when supplied DISPLAY_NAME has full path...?
 # TODO: test when FINGERPRINT protocol is unknown
 
 
