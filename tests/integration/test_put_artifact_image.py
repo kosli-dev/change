751eb7d9e3910ae4658e87a81b1e87f0a5308aaa
Arstanaly Rysbekov <arstanrys@gmail.com>
2021-02-07 15:52:37 +0100

    Update put_artifact_image() test data for dual-tests

--- tests/integration/test_put_artifact_image.py
+++ tests/integration/test_put_artifact_image.py
@@ -61,12 +61,14 @@ def test_required_env_vars_uses_CDB_ARTIFACT_DOCKER_IMAGE(capsys, mocker):
 
 def test_required_env_vars_uses_CDB_ARTIFACT_SHA(capsys, mocker):
     # artifact sha comes direct from CDB_ARTIFACT_SHA
+    commit = "92037940e4e7503055d8a8eea87e177f04f14616"
+    sha256 = "b7cdaef69c676c2466571d3233380d559ccc2032b258fc5e73f99a103db462ef"
     env = {
         "CDB_API_TOKEN": "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
-        "CDB_ARTIFACT_SHA": "b7cdaef69c676c2466571d3233380d559ccc2032b258fc5e73f99a103db462ef",
+        "CDB_ARTIFACT_SHA": sha256,
         "CDB_IS_COMPLIANT": "TRUE",
-        "CDB_ARTIFACT_GIT_URL": "http://github/me/project/commit/12037940e4e7503055d8a8eea87e177f04f14616",
-        "CDB_ARTIFACT_GIT_COMMIT": "92037940e4e7503055d8a8eea87e177f04f14616",
+        "CDB_ARTIFACT_GIT_URL": f"http://github/me/project/commit/{commit}",
+        "CDB_ARTIFACT_GIT_COMMIT": commit,
     }
     set_env_vars = {}
 
@@ -74,23 +76,81 @@ def test_required_env_vars_uses_CDB_ARTIFACT_SHA(capsys, mocker):
         put_artifact_image("tests/integration/test-pipefile.json")
     verify_approval(capsys, ["out"])
 
+    # extract data from approved cdb text file
+    this_test = "test_required_env_vars_uses_CDB_ARTIFACT_SHA"
+    approved = f"{APPROVAL_DIR}/{APPROVAL_FILE}.{this_test}.approved.txt"
+    with open(approved) as file:
+        old_approval = file.read()
+    _old_blurb, old_method, old_payload, old_url = extract_blurb_method_payload_url(old_approval)
+
+    domain = "app.compliancedb.com"
+    owner = "compliancedb"
+    name = "cdb-controls-test-pipeline"
+
+    expected_method = "Putting"
+    expected_url = f"https://{domain}/api/v1/projects/{owner}/{name}/artifacts/"
+    expected_payload = {
+        "build_url": "BUILD_URL_UNDEFINED",
+        "commit_url": f"http://github/me/project/commit/{commit}",
+        "description": "Created by build UNDEFINED",
+        "filename": "NO_DOCKER_IMAGE_FOUND",
+        "git_commit": commit,
+        "is_compliant": True,
+        "sha256": sha256
+    }
+
+    # verify data from approved cdb text file
+    assert old_method == expected_method
+    assert old_url == expected_url
+    assert old_payload == expected_payload
 
 def test_all_env_vars(capsys):
     # artifact sha comes direct from CDB_ARTIFACT_SHA
+    build_url = "https://gitlab/build/351"
+    commit = "82037940e4e7503055d8a8eea87e177f04f14616"
+    sha256="b7cdaef69c676c2466571d9933380d559ccc2032b258fc5e73f99a103db462ef"
     env = {
-        "CDB_HOST": "http://test.compliancedb.com",  # optional
+        "CDB_HOST": "https://app.compliancedb.com",  # optional
         "CDB_API_TOKEN": "6599831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
-        "CDB_ARTIFACT_SHA": "b7cdaef69c676c2466571d9933380d559ccc2032b258fc5e73f99a103db462ef",
+        "CDB_ARTIFACT_SHA": sha256,
         "CDB_IS_COMPLIANT": "TRUE",
-        "CDB_ARTIFACT_GIT_URL": "http://github/me/project/commit/12037940e4e7503055d8a8eea87e177f04f14616",
-        "CDB_ARTIFACT_GIT_COMMIT": "82037940e4e7503055d8a8eea87e177f04f14616",
-        "CDB_CI_BUILD_URL": "https://gitlab/build/351",  # optional
+        "CDB_ARTIFACT_GIT_URL": f"http://github/me/project/commit/{commit}",
+        "CDB_ARTIFACT_GIT_COMMIT": commit,
+        "CDB_CI_BUILD_URL": build_url,  # optional
     }
     set_env_vars = {}
 
     with ScopedEnvVars({**CDB_DRY_RUN, **env}, set_env_vars):
         put_artifact_image("tests/integration/test-pipefile.json")
     verify_approval(capsys, ["out"])
+
+    # extract data from approved cdb text file
+    this_test = "test_all_env_vars"
+    approved = f"{APPROVAL_DIR}/{APPROVAL_FILE}.{this_test}.approved.txt"
+    with open(approved) as file:
+        old_approval = file.read()
+    _old_blurb, old_method, old_payload, old_url = extract_blurb_method_payload_url(old_approval)
+
+    domain = "app.compliancedb.com"
+    owner = "compliancedb"
+    name = "cdb-controls-test-pipeline"
+
+    expected_method = "Putting"
+    expected_url = f"https://{domain}/api/v1/projects/{owner}/{name}/artifacts/"
+    expected_payload = {
+        "build_url": build_url,
+        "commit_url": f"http://github/me/project/commit/{commit}",
+        "description": "Created by build UNDEFINED",
+        "filename": "NO_DOCKER_IMAGE_FOUND",
+        "git_commit": commit,
+        "is_compliant": True,
+        "sha256": sha256
+    }
+
+    # verify data from approved cdb text file
+    assert old_method == expected_method
+    assert old_url == expected_url
+    assert old_payload == expected_payload
 
 
 def test_no_env_vars_raises_DockerException():
