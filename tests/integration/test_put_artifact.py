8212e11e1851d9a23d99d9d7c0c102da599231c9
Arstanaly Rysbekov <arstanrys@gmail.com>
2021-02-07 16:46:44 +0100

    Update put_artifact() test data for dual-tests

--- tests/integration/test_put_artifact.py
+++ tests/integration/test_put_artifact.py
@@ -1,23 +1,31 @@
 from cdb.put_artifact import put_artifact
 
-from tests.utils import ScopedEnvVars, CDB_DRY_RUN, verify_approval, silent
+from tests.utils import *
 
+APPROVAL_DIR = "tests/integration/approved_executions"
+APPROVAL_FILE = "test_put_artifact"
 
 def test_all_env_vars_uses_FILENAME_and_SHA(capsys):
     # Provides CDB_ARTIFACT_FILENAME and CDB_ARTIFACT_SHA.
     # Assumption is that CDB_ARTIFACT_FILENAME is naming a file
     # that is not volume-mounted, so the sha cannot be calculated
     # from the file, so SHA is passed too.
+
+    commit = "abc50c8a53f79974d615df335669b59fb56a4ed4"
+    artifact_name = "door-is-a.jar"
+    sha256 = "444daef69c676c2466571d3211180d559ccc2032b258fc5e73f99a103db462ef"
+    build_url = "https://gitlab/build/2156"
+
     env = {
-        "CDB_HOST": "https://test.compliancedb.com",
+        "CDB_HOST": "https://app.compliancedb.com",
         "CDB_API_TOKEN": "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
-        "CDB_ARTIFACT_FILENAME": "door-is-a.jar",
-        "CDB_ARTIFACT_SHA": "444daef69c676c2466571d3211180d559ccc2032b258fc5e73f99a103db462ef",
+        "CDB_ARTIFACT_FILENAME": artifact_name,
+        "CDB_ARTIFACT_SHA": sha256,
         "CDB_IS_COMPLIANT": "TRUE",
-        "CDB_ARTIFACT_GIT_URL": "https://github/me/project/commit/abc50c8a53f79974d615df335669b59fb56a4ed4",
-        "CDB_ARTIFACT_GIT_COMMIT": "abc50c8a53f79974d615df335669b59fb56a4ed4",
-        "CDB_CI_BUILD_URL": "https://gitlab/build/2156",
-        "CDB_BUILD_NUMBER": "751"
+        "CDB_ARTIFACT_GIT_URL": f"https://github/me/project/commit/{commit}",
+        "CDB_ARTIFACT_GIT_COMMIT": commit,
+        "CDB_CI_BUILD_URL": build_url,
+        "CDB_BUILD_NUMBER": "2156"
     }
     set_env_vars = {}
 
@@ -25,27 +33,86 @@ def test_all_env_vars_uses_FILENAME_and_SHA(capsys):
         put_artifact("tests/integration/test-pipefile.json")
     verify_approval(capsys, ["out"])
 
+    # extract data from approved cdb text file
+    this_test = "test_all_env_vars_uses_FILENAME_and_SHA"
+    approved = f"{APPROVAL_DIR}/{APPROVAL_FILE}.{this_test}.approved.txt"
+    with open(approved) as file:
+        old_approval = file.read()
+    _old_blurb, old_method, old_payload, old_url = extract_blurb_method_payload_url(old_approval)
+
+    domain = "app.compliancedb.com"
+    owner = "compliancedb"
+    name = "cdb-controls-test-pipeline"
+
+    expected_method = "Putting"
+    expected_url = f"https://{domain}/api/v1/projects/{owner}/{name}/artifacts/"
+    expected_payload = {
+        "build_url": build_url,
+        "commit_url": f"https://github/me/project/commit/{commit}",
+        "description": "Created by build 2156",
+        "filename": artifact_name,
+        "git_commit": commit,
+        "is_compliant": True,
+        "sha256": sha256
+    }
+
+    # verify data from approved cdb text file
+    assert old_method == expected_method
+    assert old_url == expected_url
+    assert old_payload == expected_payload
+
 
 def test_required_env_vars_uses_CDB_ARTIFACT_FILENAME(capsys, mocker):
     # Does not provide CDB_ARTIFACT_SHA
     # Assumption is that CDB_ARTIFACT_FILENAME names a file
     # that is volume-mounted and the CDB_ARTIFACT_SHA is calculated.
+    commit = "abc50c8a53f79974d615df335669b59fb56a4ed3"
+    build_url = "https://gitlab/build/1456"
+    artifact_name = "jam.jar"
     env = {
         "CDB_API_TOKEN": "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
-        "CDB_ARTIFACT_FILENAME": "jam.jar",
+        "CDB_ARTIFACT_FILENAME": artifact_name,
         "CDB_IS_COMPLIANT": "TRUE",
-        "CDB_ARTIFACT_GIT_URL": "http://github/me/project/commit/abc50c8a53f79974d615df335669b59fb56a4ed3",
-        "CDB_ARTIFACT_GIT_COMMIT": "abc50c8a53f79974d615df335669b59fb56a4ed3",
-        "CDB_CI_BUILD_URL": "https://gitlab/build/1456",
-        "CDB_BUILD_NUMBER": "349"
+        "CDB_ARTIFACT_GIT_URL": f"https://github/me/project/commit/{commit}",
+        "CDB_ARTIFACT_GIT_COMMIT": commit,
+        "CDB_CI_BUILD_URL": build_url,
+        "CDB_BUILD_NUMBER": "1456"
     }
-    sha = "ddcdaef69c676c2466571d3288880d559ccc2032b258fc5e73f99a103db462ee"
-    set_env_vars = {'CDB_ARTIFACT_SHA': sha}
+    sha256 = "ddcdaef69c676c2466571d3288880d559ccc2032b258fc5e73f99a103db462ee"
+    set_env_vars = {'CDB_ARTIFACT_SHA': sha256}
 
     with ScopedEnvVars({**CDB_DRY_RUN, **env}, set_env_vars):
-        mocker.patch('cdb.cdb_utils.calculate_sha_digest_for_file', return_value=sha)
+        mocker.patch('cdb.cdb_utils.calculate_sha_digest_for_file', return_value=sha256)
         put_artifact("tests/integration/test-pipefile.json")
     verify_approval(capsys, ["out"])
+
+    # extract data from approved cdb text file
+    this_test = "test_required_env_vars_uses_CDB_ARTIFACT_FILENAME"
+    approved = f"{APPROVAL_DIR}/{APPROVAL_FILE}.{this_test}.approved.txt"
+    with open(approved) as file:
+        old_approval = file.read()
+    _old_blurb, old_method, old_payload, old_url = extract_blurb_method_payload_url(old_approval)
+
+    domain = "app.compliancedb.com"
+    owner = "compliancedb"
+    name = "cdb-controls-test-pipeline"
+
+    expected_method = "Putting"
+    expected_url = f"https://{domain}/api/v1/projects/{owner}/{name}/artifacts/"
+    expected_payload = {
+        "build_url": build_url,
+        "commit_url": f"https://github/me/project/commit/{commit}",
+        "description": "Created by build 1456",
+        "filename": artifact_name,
+        "git_commit": commit,
+        "is_compliant": True,
+        "sha256": sha256
+    }
+
+    # verify data from approved cdb text file
+    assert old_method == expected_method
+    assert old_url == expected_url
+    assert old_payload == expected_payload
 
 
 def test_CDB_ARTIFACT_FILENAME_is_missing(capsys):
