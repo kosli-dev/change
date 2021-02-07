f9b40d34e98693bf54d330e736cf1d96fb6fe306
Jon Jagger <jon@jaggersoft.com>
2021-02-07 20:01:38 +0000

    Use env_var decorator to collect env-var properties in LogArtifact() command

--- commands/command.py
+++ commands/command.py
@@ -1,6 +1,17 @@
+from collections import namedtuple
 import json
 from commands import CommandError, DefaultedEnvVar, OptionalEnvVar, RequiredEnvVar
 from commands import FingerprintEnvVar, DisplayNameEnvVar
+
+
+def make_env_var_registrar():
+    registry = {}
+
+    def registrar(func):
+        registry[func.__name__] = func
+        return func
+    registrar.all = registry
+    return registrar
 
 
 class Command:
@@ -13,40 +24,53 @@ class Command:
     def __call__(self):
         raise NotImplementedError("Command subclass override missing")
 
-    @property
-    def env(self):
-        return self._context.env
+    env_var = make_env_var_registrar()
 
     @property
-    def file_fingerprinter(self):
-        return self._context.file_fingerprinter
+    def env_vars(self):
+        names = []
+        props = []
+        for name, func in Command.env_var.all.items():
+            names.append(name)
+            props.append(func(self))
+        return namedtuple('EnvVars', tuple(names))(*props)
 
     @property
-    def docker_fingerprinter(self):
-        return self._context.docker_fingerprinter
-
-    @property
+    @env_var
     def name(self):
         description = "The Merkely command to execute."
         return self._required_env_var("COMMAND", description)
 
     @property
+    @env_var
     def api_token(self):
         description = "Your API token for Merkely."
         return self._required_env_var("API_TOKEN", description)
 
     @property
+    @env_var
     def display_name(self):
         return DisplayNameEnvVar(self)
 
     @property
+    @env_var
     def fingerprint(self):
         return FingerprintEnvVar(self)
 
     @property
+    @env_var
+    def host(self):
+        default = "https://app.compliancedb.com"
+        description = f"The host name for Merkely. The default is {default}"
+        return self._defaulted_env_var("HOST", default, description)
+
+    @property
+    @env_var
     def is_compliant(self):
         description = "Whether this artifact is considered compliant from you build process."
         return self._required_env_var('IS_COMPLIANT', description)
+
+    # - - - - - - - - - - - - - - - - - - - - -
 
     @property
     def merkelypipe(self):
@@ -61,11 +85,21 @@ class Command:
         except json.decoder.JSONDecodeError as exc:
             raise CommandError(f"{filename} invalid json - {str(exc)}")
 
+    # - - - - - - - - - - - - - - - - - - - - -
+
     @property
-    def host(self):
-        default = "https://app.compliancedb.com"
-        description = f"The host name for Merkely. The default is {default}"
-        return self._defaulted_env_var("HOST", default, description)
+    def env(self):
+        return self._context.env
+
+    @property
+    def file_fingerprinter(self):
+        return self._context.file_fingerprinter
+
+    @property
+    def docker_fingerprinter(self):
+        return self._context.docker_fingerprinter
+
+    # - - - - - - - - - - - - - - - - - - - - -
 
     def _defaulted_env_var(self, name, default, description):
         return DefaultedEnvVar(self.env, f"MERKELY_{name}", default, description)
