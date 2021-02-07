1db0f4b909ce3929f2e55898ddb5c3f98ff1b18a
Jon Jagger <jon@jaggersoft.com>
2021-02-07 08:51:14 +0000

    Tidy Fingerprinter() tests

--- tests/unit/test_m_fingerprinter.py
+++ tests/unit/test_m_fingerprinter.py
@@ -4,48 +4,46 @@ from commands import CommandError, Fingerprinter, RequiredEnvVar
 from tests.utils import *
 from pytest import raises
 
+SHA256 = "ddee5566dc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
+
 
 def test_docker_protocol():
     image_name = "acme/road-runner:4.8"
-    sha256 = "ddee5566dc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
 
     fingerprint = f"docker://{image_name}"
     env_vars = make_env_vars(fingerprint, None)
 
-    fingerprinter = MockImageFingerprinter(image_name, sha256)
-    expected = sha256, image_name
+    fingerprinter = MockImageFingerprinter(image_name, SHA256)
+    expected = SHA256, image_name
 
     assert fingerprinter(env_vars) == expected
 
 
 def test_file_protocol_for_unpathed_filename_is_at_root():
     filename = "jam.jar"
-    sha256 = "ddee5566dc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
 
     fingerprint = f"file://{filename}"
     env_vars = make_env_vars(fingerprint, None)
 
-    fingerprinter = MockFileFingerprinter(filename, sha256)
-    expected = sha256, filename
+    fingerprinter = MockFileFingerprinter(filename, SHA256)
+    expected = SHA256, filename
 
     assert fingerprinter(env_vars) == expected
 
 
 def test_file_protocol_for_pathed_filename_uses_display_name_of_unpathed_filename():
     directory, filename = "/user/data", "jam.jar"
-    sha256 = "ddee5566dc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
 
     fingerprint = f"file://{directory}/{filename}"
     env_vars = make_env_vars(fingerprint, None)
 
-    expected = sha256, filename
-    assert MockFileFingerprinter(f"{directory}/{filename}", sha256)(env_vars) == expected
+    expected = SHA256, filename
+    assert MockFileFingerprinter(f"{directory}/{filename}", SHA256)(env_vars) == expected
 
 
 def test_unknown_protocol_raises():
-    protocol = "ash256://"
-    sha256 = "ddee5566dc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
-    fingerprint = f"{protocol}{sha256}"
+    protocol = "xyz://"
+    fingerprint = f"{protocol}{SHA256}"
     image_name = "acme/road-runner:4.8"
     env_vars = make_env_vars(fingerprint, image_name)
 
@@ -63,12 +61,13 @@ def make_env_vars(fingerprint, display_name):
     if display_name is not None:
         env["MERKELY_DISPLAY_NAME"] = display_name
 
+    description = ""
     return namedtuple('EnvVars', (
         'fingerprint',
         'display_name'
     ))(
-        RequiredEnvVar('MERKELY_FINGERPRINT', env, "description"),
-        RequiredEnvVar('MERKELY_DISPLAY_NAME', env, "description")
+        RequiredEnvVar('MERKELY_FINGERPRINT', env, description),
+        RequiredEnvVar('MERKELY_DISPLAY_NAME', env, description)
     )
 
 # Further tests
