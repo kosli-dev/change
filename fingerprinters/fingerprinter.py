d5f57274b0c88bb319b96b5803c5c6002d37ed81
Jon Jagger <jon@jaggersoft.com>
2021-02-09 19:19:05 +0000

    Collapse test duplication

--- fingerprinters/fingerprinter.py
+++ fingerprinters/fingerprinter.py
@@ -3,21 +3,21 @@ class Fingerprinter:
 
     def __call__(self, protocol, artifact_name):
         print(self._calculating_message(protocol, artifact_name))
-        repo_digest = self._fingerprint(artifact_name)
-        print(self._calculated_message(repo_digest))
-        return repo_digest
+        sha256 = self._fingerprint(artifact_name)
+        print(self._calculated_message(sha256))
+        return sha256
 
     def _fingerprint(self, artifact_name):
-        raise NotImplementedError(f"{self.class_name}._fingerprint(...) subclass override missing")
+        raise NotImplementedError(f"{self._class_name}._fingerprint(...) subclass override missing")
 
     @staticmethod
     def _calculating_message(protocol, artifact_name):
         return f"Calculating fingerprint for {protocol}{artifact_name}"
 
     @staticmethod
-    def _calculated_message(sha):
-        return f"Calculated fingerprint: {sha}"
+    def _calculated_message(sha256):
+        return f"Calculated fingerprint: {sha256}"
 
     @property
-    def class_name(self):
+    def _class_name(self):
         return self.__class__.__name__
--- tests/unit/test_m_fingerprint_env_var.py
+++ tests/unit/test_m_fingerprint_env_var.py
@@ -7,26 +7,18 @@ SHA256 = "ddee5566dc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
 
 
 def test_unknown_protocol__all_properties_raise():
-    fingerprint = f"ash256://{SHA256}"
-    env = {"MERKELY_FINGERPRINT": fingerprint}
-    context = Context(env, None, None)
-    command = Command(context)
-    fev = FingerprintEnvVar(command)
+    def assert_bad_property_raises(protocol, property_name):
+        fingerprint = f"{protocol}{SHA256}"
+        env = {"MERKELY_FINGERPRINT": fingerprint}
+        context = Context(env, None, None)
+        command = Command(context)
+        ev = FingerprintEnvVar(command)
+        with raises(CommandError) as exc:
+            getattr(ev, property_name)
+        assert str(exc.value) == f"Unknown protocol: {fingerprint}"
 
-    expected_diagnostic = f"Unknown protocol: {fingerprint}"
-
-    with raises(CommandError) as exc:
-        fev.value
-    assert str(exc.value) == expected_diagnostic
-
-    with raises(CommandError) as exc:
-        fev.protocol
-    assert str(exc.value) == expected_diagnostic
-
-    with raises(CommandError) as exc:
-        fev.sha
-    assert str(exc.value) == expected_diagnostic
-
-    with raises(CommandError) as exc:
-        fev.artifact_name
-    assert str(exc.value) == expected_diagnostic
+    for bad_protocol in ['ash256://', 'not_even_a_colon']:
+        assert_bad_property_raises(bad_protocol, 'value')
+        assert_bad_property_raises(bad_protocol, 'protocol')
+        assert_bad_property_raises(bad_protocol, 'sha')
+        assert_bad_property_raises(bad_protocol, 'artifact_name')
