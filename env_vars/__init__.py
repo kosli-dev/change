e3b43084f009950657915e1b01d512f075a5089d
Jon Jagger <jon@jaggersoft.com>
2021-02-13 10:42:21 +0000

    Switch rest of fingerprint-env-var code into fingerprinter classes

--- env_vars/__init__.py
+++ env_vars/__init__.py
@@ -4,12 +4,6 @@ from .required_env_var import RequiredEnvVar
 from .defaulted_env_var import DefaultedEnvVar
 from .static_defaulted_env_var import StaticDefaultedEnvVar
 
-from .fingerprint_env_var_cls_for import fingerprint_env_var_cls_for
-
 from .fingerprint_env_var import FingerprintEnvVar
-
-from .docker_fingerprint_env_var import DockerFingerprintEnvVar
-from .file_fingerprint_env_var import FileFingerprintEnvVar
-from .sha256_fingerprint_env_var import Sha256FingerprintEnvVar
 from .host_env_var import HostEnvVar
 from .user_data_env_var import UserDataEnvVar
--- env_vars/fingerprint_env_var.py
+++ env_vars/fingerprint_env_var.py
@@ -1,6 +1,5 @@
 from commands import CommandError
 from env_vars import RequiredEnvVar
-from env_vars import fingerprint_env_var_cls_for
 
 import re
 
@@ -26,23 +25,22 @@ class FingerprintEnvVar(RequiredEnvVar):
 
     @property
     def protocol(self):
-        unknown_protocol_error = CommandError(f"Unknown protocol: {self.string}")
-        regex = re.compile(r'(?P<protocol>[0-9a-z]+:\/\/).*')
-        match = regex.match(self.string)
-        if match is None:
-            raise unknown_protocol_error
-        protocol = match.group('protocol')
-        if fingerprint_env_var_cls_for(protocol) is None:
-            raise unknown_protocol_error
-        return protocol
+        f = self.__fingerprinter
+        return f.protocol
 
     @property
     def artifact_name(self):
-        return self.__fingerprint.artifact_name
+        f = self.__fingerprinter
+        p = f.protocol
+        after = self.string[len(p):]
+        return f.artifact_name(after)
 
     @property
     def artifact_basename(self):
-        return self.__fingerprint.artifact_basename
+        f = self.__fingerprinter
+        p = f.protocol
+        after = self.string[len(p):]
+        return f.artifact_basename(after)
 
     @property
     def sha(self):
@@ -50,14 +48,6 @@ class FingerprintEnvVar(RequiredEnvVar):
         p = f.protocol
         after = self.string[len(p):]
         return f.sha(p, after)
-
-    @property
-    def __fingerprint(self):
-        after_protocol = self.string[len(self.protocol):]
-        if after_protocol == "":
-            raise CommandError(f"Empty {self.protocol} fingerprint")
-        cls = fingerprint_env_var_cls_for(self.protocol)
-        return cls(self.__command, self.protocol, after_protocol)
 
     @property
     def __fingerprinter(self):
