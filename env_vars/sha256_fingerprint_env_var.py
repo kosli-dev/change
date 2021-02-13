ebc7a0a5140a27a0dd79d0b1996cbc6212a253fc
Jon Jagger <jon@jaggersoft.com>
2021-02-13 09:33:09 +0000

    Move all of sha256 env-var code into fingerprinter

--- env_vars/sha256_fingerprint_env_var.py
+++ env_vars/sha256_fingerprint_env_var.py
@@ -1,8 +1,3 @@
-from collections import namedtuple
-from commands import CommandError
-import re
-
-
 class Sha256FingerprintEnvVar:
 
     def __init__(self, command, protocol, sha_and_artifact_name):
@@ -12,24 +7,12 @@ class Sha256FingerprintEnvVar:
 
     @property
     def artifact_name(self):
-        return self.__validated.artifact_name
+        return self.__command.sha256_fingerprinter.artifact_name(self.__sha_and_artifact_name)
 
     @property
     def artifact_basename(self):
-        return self.artifact_name
+        return self.__command.sha256_fingerprinter.artifact_basename(self.__sha_and_artifact_name)
 
     @property
     def sha(self):
         return self.__command.sha256_fingerprinter.sha(self.__protocol, self.__sha_and_artifact_name)
-
-    __REGEX = re.compile(r'(?P<sha>[0-9a-f]{64})\/(?P<artifact_name>.+)')
-
-    @property
-    def __validated(self):
-        string = self.__sha_and_artifact_name
-        match = self.__REGEX.match(string)
-        if match is None:
-            raise CommandError(f"Invalid {self.__protocol} fingerprint: {string}")
-        names = ('sha', 'artifact_name')
-        args = (match.group('sha'), match.group('artifact_name'))
-        return namedtuple('Both', names)(*args)
--- fingerprinters/sha256_fingerprinter.py
+++ fingerprinters/sha256_fingerprinter.py
@@ -17,6 +17,12 @@ class Sha256Fingerprinter(Fingerprinter):
             '    ...',
         ])
 
+    def artifact_name(self, sha_and_artifact_name):
+        return self.__validated(sha_and_artifact_name).artifact_name
+
+    def artifact_basename(self, sha_and_artifact_name):
+        return self.artifact_name(sha_and_artifact_name)
+
     def sha(self, protocol, sha_and_artifact_name):
         return self.__validated(sha_and_artifact_name).sha
 
