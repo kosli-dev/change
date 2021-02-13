68c135e6d966bf571dd35480cc92c2089e016c20
Jon Jagger <jon@jaggersoft.com>
2021-02-13 08:33:29 +0000

    Delete dead code

--- fingerprinters/fingerprinter.py
+++ fingerprinters/fingerprinter.py
@@ -2,18 +2,8 @@
 class Fingerprinter:
 
     def __call__(self, protocol, artifact_name):
-        #print(self._calculating_message(protocol, artifact_name))
         sha256 = self._fingerprint(artifact_name)
-        #print(self._calculated_message(sha256))
         return sha256
 
     def _fingerprint(self, artifact_name):  # pragma: no cover
         raise NotImplementedError
-
-    @staticmethod
-    def _calculating_message(protocol, artifact_name):
-        return f"Calculating fingerprint for {protocol}{artifact_name}"
-
-    @staticmethod
-    def _calculated_message(sha256):
-        return f"Calculated fingerprint: {sha256}"
