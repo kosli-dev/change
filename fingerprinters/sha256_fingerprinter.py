d69cd221fb8972a89b651f174c9237929eac109a
Jon Jagger <jon@jaggersoft.com>
2021-02-13 16:44:48 +0000

    Refactoring

--- fingerprinters/sha256_fingerprinter.py
+++ fingerprinters/sha256_fingerprinter.py
@@ -25,25 +25,23 @@ class Sha256Fingerprinter(Fingerprinter):
         return string.startswith(PROTOCOL)
 
     def artifact_basename(self, string):
-        return self.artifact_name(string)
+        return self.__validated(string).artifact_name
 
     def artifact_name(self, string):
-        assert self.handles_protocol(string)
-        sha_and_artifact_name = string[len(PROTOCOL):]
-        return self.__validated(sha_and_artifact_name).artifact_name
+        return self.__validated(string).artifact_name
 
     def sha(self, string):
-        assert self.handles_protocol(string)
-        sha_and_artifact_name = string[len(PROTOCOL):]
-        return self.__validated(sha_and_artifact_name).sha
+        return self.__validated(string).sha
 
     __REGEX = re.compile(r'(?P<sha>[0-9a-f]{64})\/(?P<artifact_name>.+)')
 
-    def __validated(self, sha_and_artifact_name):
-        match = self.__REGEX.match(sha_and_artifact_name)
+    def __validated(self, string):
+        assert self.handles_protocol(string)
+        after_protocol = string[len(PROTOCOL):]
+        match = self.__REGEX.match(after_protocol)
         if match is None:
             from commands import CommandError
-            raise CommandError(f"Invalid {PROTOCOL} fingerprint: {sha_and_artifact_name}")
+            raise CommandError(f"Invalid {PROTOCOL} fingerprint: {after_protocol}")
         names = ('sha', 'artifact_name')
         args = (match.group('sha'), match.group('artifact_name'))
         return namedtuple('Both', names)(*args)
