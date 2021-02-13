e0b16dce486f40973064fe91ca3ca042e7e9ca20
Jon Jagger <jon@jaggersoft.com>
2021-02-13 06:43:39 +0000

    Collapse FingerprintEnvVar test duplication

--- tests/unit/test_m_fingerprint_env_var.py
+++ tests/unit/test_m_fingerprint_env_var.py
@@ -5,28 +5,33 @@ from pytest import raises
 
 SHA256 = "ddee5566dc05772d90dc6929ad4f1fbc14aa105addf3326aa5cf575a104f51dc"
 
+
 def test_docker_protocol_properties():
     protocol = "docker://"
     image_name = "acme/road-runner:4.5"
     fingerprint = f"{protocol}/{image_name}"
-    env = {"MERKELY_FINGERPRINT": fingerprint}
-    context = Context(env, None, None)
-    command = Command(context)
-    ev = FingerprintEnvVar(command)
+    ev = make_fingerprint_env_var(fingerprint)
     assert ev.protocol == protocol
     assert ev.artifact_name == image_name
     assert ev.artifact_basename == image_name
 
 
-def test_file_protocol_properties():
+def test_file_protocol__file_in_root_dir_properties():
+    protocol = "file://"
+    filename = "jam.jar"
+    fingerprint = f"{protocol}/{filename}"
+    ev = make_fingerprint_env_var(fingerprint)
+    assert ev.protocol == protocol
+    assert ev.artifact_name == filename
+    assert ev.artifact_basename == filename
+
+
+def test_file_protocol__file_in_sub_dir_properties():
     protocol = "file://"
     directory = "/user/data"
     filename = "jam.jar"
     fingerprint = f"{protocol}/{directory}/{filename}"
-    env = {"MERKELY_FINGERPRINT": fingerprint}
-    context = Context(env, None, None)
-    command = Command(context)
-    ev = FingerprintEnvVar(command)
+    ev = make_fingerprint_env_var(fingerprint)
     assert ev.protocol == protocol
     assert ev.artifact_name == f"{directory}/{filename}"
     assert ev.artifact_basename == filename
@@ -35,10 +40,7 @@ def test_file_protocol_properties():
 def test_unknown_protocol__all_properties_raise():
     def assert_raises(unknown_protocol, property_name):
         fingerprint = f"{unknown_protocol}{SHA256}/jam.jar"
-        env = {"MERKELY_FINGERPRINT": fingerprint}
-        context = Context(env, None, None)
-        command = Command(context)
-        ev = FingerprintEnvVar(command)
+        ev = make_fingerprint_env_var(fingerprint)
         with raises(CommandError) as exc:
             getattr(ev, property_name)
         assert str(exc.value) == f"Unknown protocol: {fingerprint}"
@@ -48,3 +50,10 @@ def test_unknown_protocol__all_properties_raise():
         assert_raises(protocol, 'protocol')
         assert_raises(protocol, 'sha')
         assert_raises(protocol, 'artifact_name')
+
+
+def make_fingerprint_env_var(fingerprint):
+    env = {"MERKELY_FINGERPRINT": fingerprint}
+    context = Context(env, None, None)
+    command = Command(context)
+    return FingerprintEnvVar(command)
