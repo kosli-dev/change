e0734d8d6394594d9ca2d403023c17531eb96d63
Jon Jagger <jon@jaggersoft.com>
2021-02-14 11:33:01 +0000

    Simplify command-builder

--- commands/command_builder.py
+++ commands/command_builder.py
@@ -11,19 +11,10 @@ COMMANDS = {
 }
 
 
-class UnknownCommand(Command):
-
-    def __call__(self):
-        raise self.unknown_command_error()
-
-    @property
-    def _env_var_names(self):
-        raise self.unknown_command_error()
-
-    def unknown_command_error(self):
-        return CommandError(f"Unknown command: {self.name.value}")
-
-
 def build_command(context):
     name = Command(context).name.value
-    return COMMANDS.get(name, UnknownCommand)(context)
+    klass = COMMANDS.get(name)
+    if klass is not None:
+        return klass(context)
+    else:
+        raise CommandError(f"Unknown command: {name}")
