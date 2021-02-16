6c7eaf1836ab8b0052ead304053d7c95dd9e2ae7
Arstanaly Rysbekov <arstanrys@gmail.com>
2021-02-16 20:28:25 +0100

    Add approval-test for put_bitbucket_pull_request()

--- tests/integration/test_bitbucket.py
+++ tests/integration/test_bitbucket.py
@@ -8,14 +8,6 @@ mocked_bitbucket_pull_requests_api_response = {
         "links": {
             "self": {"href": "test_self_uri", "name": "test_self"},
             "html": {"href": "test_html_uri", "name": "test_html"},
-            "commits": {"href": "test_commit_uri", "name": "test_commits"},
-            "approve": {"href": "test_approve_uri", "name": "test_approve"},
-            "diff": {"href": "test_diff_uri", "name": "test_diff"},
-            "diffstat": {"href": "test_diffstat_uri", "name": "test_diffstat"},
-            "comments": {"href": "test_comments_uri", "name": "test_comments"},
-            "activity": {"href": "test_activity_uri", "name": "test_activity"},
-            "merge": {"href": "test_merge_uri", "name": "test_merge"},
-            "decline": {"href": "test_decline_uri", "name": "test_decline"}
         },
         "id": "1",
         "title": "test pull request",
@@ -23,7 +15,6 @@ mocked_bitbucket_pull_requests_api_response = {
     "state": "OPEN",
     "participants": [
         {
-            "role": "PARTICIPANT",
             "approved": True,
             "state": "approved",
             "user": {
@@ -48,6 +39,7 @@ test_pipefile = {
 
 
 def test_only_required_env_vars(capsys, mocker):
+    # Test for put_bitbucket_pull_request
     env = {
         "CDB_API_TOKEN": "1239831f4ee3b79e7c5b7e0ebe75d67aa66e7aab",
         "BITBUCKET_API_TOKEN": "6199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
@@ -69,6 +61,7 @@ def test_only_required_env_vars(capsys, mocker):
 
 
 def test_all_env_vars(capsys, mocker):
+    # Test for put_bitbucket_pull_request
     env = {
         "CDB_API_TOKEN": "1239831f4ee3b79e7c5b7e0ebe75d67aa66e7aab",
         "BITBUCKET_API_TOKEN": "6199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
@@ -92,7 +85,42 @@ def test_all_env_vars(capsys, mocker):
     verify_approval(capsys, ["out"])
 
 
+def test_put_bitbucket_pull_request_with_no_approvers_in_pr(capsys, mocker):
+    # Test for put_bitbucket_pull_request when there are  0 approvers in Bitbucket GET/pullrequests response
+    bitbucket_pr_response_without_participants = {
+        "values": [{
+            "links": {
+                "self": {"href": "test_self_uri", "name": "test_self"},
+                "html": {"href": "test_html_uri", "name": "test_html"},
+            },
+            "id": "1",
+            "title": "test pull request",
+        }],
+        "state": "OPEN",
+        "participants": []
+    }
+    env = {
+        "CDB_API_TOKEN": "1239831f4ee3b79e7c5b7e0ebe75d67aa66e7aab",
+        "BITBUCKET_API_TOKEN": "6199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
+        "CDB_ARTIFACT_SHA": "b7cdaef69c676c2466571d3233380d559ccc2032b258fc5e73f99a103db462ef",
+        "BITBUCKET_WORKSPACE": "test_project",
+        "BITBUCKET_REPO_SLUG": "test_repo",
+        "BITBUCKET_COMMIT": "12037940e4e7503055d8a8eea87e177f04f14616",
+        "BITBUCKET_API_USER": "test_user",
+    }
+
+    with ScopedEnvVars({**CDB_DRY_RUN, **env}):
+        mocker.patch('cdb.bitbucket.requests.get',
+                     return_value=MockedAPIResponse(200, bitbucket_pr_response_without_participants))
+        mocker.patch('cdb.bitbucket.json.loads', return_value=bitbucket_pr_response_without_participants)
+        mocker.patch('cdb.cdb_utils.load_project_configuration', return_value=test_pipefile)
+        put_bitbucket_pull_request("tests/integration/test-pipefile.json")
+        mocker.stopall()
+    verify_approval(capsys, ["out"])
+
+
 def test_exception_for_incomplete_bitbucket_api_resposne(capsys, mocker):
+    # Test for put_bitbucket_pull_request
     # Test exception message(1) for get_pull_requests_from_bitbucket_api()
     # "Repository pull requests are still being indexed, please retry."
     with pytest.raises(SystemExit) as excinfo:
@@ -103,6 +131,7 @@ def test_exception_for_incomplete_bitbucket_api_resposne(capsys, mocker):
 
 
 def test_exception_for_non_existent_bitbucket_repository(capsys, mocker):
+    # Test for put_bitbucket_pull_request
     # Test exception message(2) for get_pull_requests_from_bitbucket_api()
     # "Repository does not exists or pull requests are not indexed."
     # "Please make sure Pull Request Commit Links app is installed"
@@ -114,6 +143,7 @@ def test_exception_for_non_existent_bitbucket_repository(capsys, mocker):
 
 
 def test_exception_for_failed_pull_requests_fetching(capsys, mocker):
+    # Test for put_bitbucket_pull_request
     # Test exception message(3) for get_pull_requests_from_bitbucket_api()
     # "Exception occurred in fetching pull requests. Http return code is 401"
     with pytest.raises(SystemExit) as excinfo:
