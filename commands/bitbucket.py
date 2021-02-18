5a194fcac72089d5aa20d617fbcd3bba21f79d7e
Jon Jagger <jon@jaggersoft.com>
2021-02-18 15:03:27 +0000

    Add start of first test for LogArtifact() on bitbucket

---
+++ commands/bitbucket.py
@@ -0,0 +1,78 @@
+import sys
+from bitbucket_pipes_toolkit import Pipe, get_logger
+
+import cdb.control_junit
+import cdb.create_release
+import cdb.create_deployment
+import cdb.put_artifact
+import cdb.put_artifact_image
+import cdb.put_pipeline
+from cdb.bitbucket import get_bitbucket_repo_url, adapt_put_artifact_image_env_variables, \
+    adapt_create_release_variables, adapt_control_junit_env_variables, adapt_put_artifact_env_variables
+
+sys.path.append('/app/')
+import cdb.cdb_utils
+import cdb.bitbucket
+from cdb.cdb_utils import set_artifact_sha_env_variable_from_file_or_image
+
+
+schema = {
+    'CDB_COMMAND': {'type': 'string', 'required': True},
+    'CDB_PIPELINE_DEFINITION': {'type': 'string', 'required': True},
+    'CDB_API_TOKEN': {'type': 'string', 'required': True},
+    # PUT Pipeline Variables
+    #   ...none...
+    # Put Artifact Variables
+    'CDB_IS_COMPLIANT': {'type': 'boolean', 'required': False},
+    'CDB_ARTIFACT_GIT_URL': {'type': 'string', 'required': False},
+    'CDB_ARTIFACT_GIT_COMMIT': {'type': 'string', 'required': False},
+    'CDB_CI_BUILD_URL': {'type': 'string', 'required': False},
+    'CDB_BUILD_NUMBER': {'type': 'string', 'required': False},
+    'CDB_ARTIFACT_SHA': {'type': 'string', 'required': False},
+    'CDB_ARTIFACT_FILENAME': {'type': 'string', 'required': False},
+
+}
+
+logger = get_logger()
+
+
+class BitbucketPipe(Pipe):
+    def run(self):
+        super().run()
+
+        logger.info('Executing the pipe...')
+
+        command = self.get_variable('CDB_COMMAND')
+        pipeline_definition_file = self.get_variable('CDB_PIPELINE_DEFINITION')
+        api_token = self.get_variable('CDB_API_TOKEN')
+
+        print("Command: " + command)
+        print("Pipeline: " + pipeline_definition_file)
+        print("API_TOKEN: " + api_token)
+
+        if command == "put_pipeline":
+            cdb.put_pipeline.put_pipeline(pipeline_definition_file)
+        if command == "put_artifact":
+            adapt_put_artifact_env_variables()
+            cdb.put_artifact.put_artifact(pipeline_definition_file)
+        if command == "put_artifact_image":
+            adapt_put_artifact_image_env_variables()
+            cdb.put_artifact_image.put_artifact_image(pipeline_definition_file)
+        if command == "control_junit":
+            adapt_control_junit_env_variables()
+            cdb.control_junit.control_junit(pipeline_definition_file)
+        if command == "control_bitbucket_pr":
+            set_artifact_sha_env_variable_from_file_or_image()
+            cdb.bitbucket.put_bitbucket_pull_request(pipeline_definition_file)
+        if command == "create_release":
+            adapt_create_release_variables()
+            cdb.create_release.create_release(pipeline_definition_file)
+        if command == "create_deployment":
+            cdb.create_deployment.create_deployment(pipeline_definition_file)
+
+        self.success(message="Success!")
+
+
+if __name__ == '__main__':
+    pipe = BitbucketPipe(pipe_metadata='/pipe.yml', schema=schema)
+    pipe.run()
---
+++ tests/unit/test_m_bitbucket_log_artifact.py
@@ -0,0 +1,31 @@
+from commands.bitbucket import BitbucketPipe, schema
+
+from tests.utils import *
+
+
+def test_required_env_vars(capsys, mocker):
+    bitbucket_commit = "abc50c8a53f79974d615df335669b59fb56a4ed3"
+    env = {
+        "CDB_COMMAND": "put_artifact_image",
+        "CDB_PIPELINE_DEFINITION": "tests/data/pipefile.json",
+        "CDB_API_TOKEN": "SOME_RANDOM_TOKEN",
+        "CDB_ARTIFACT_DOCKER_IMAGE": "acme/road-runner:4.67",
+        "BITBUCKET_COMMIT": bitbucket_commit,
+        "BITBUCKET_BUILD_NUMBER": "1975",
+        "BITBUCKET_WORKSPACE": "acme",
+        "BITBUCKET_REPO_SLUG": "road-runner"
+    }
+    sha = "aacdaef69c676c2466571d3288880d559ccc2032b258fc5e73f99a103db462ee"
+    set_env_vars = {
+        'CDB_ARTIFACT_GIT_URL': 'https://bitbucket.org/acme/road-runner/commits/' + bitbucket_commit,
+        'CDB_ARTIFACT_GIT_COMMIT': bitbucket_commit,
+        'CDB_BUILD_NUMBER': '1975',
+        'CDB_CI_BUILD_URL': 'https://bitbucket.org/acme/road-runner/addon/pipelines/home#!/results/1975',
+        'CDB_ARTIFACT_SHA': 'aacdaef69c676c2466571d3288880d559ccc2032b258fc5e73f99a103db462ee'
+    }
+    with ScopedEnvVars({**CDB_DRY_RUN, **env}, set_env_vars):
+        pipe = BitbucketPipe(pipe_metadata='/pipe.yml', schema=schema)
+        mocker.patch('cdb.cdb_utils.calculate_sha_digest_for_docker_image', return_value=sha)
+        pipe.run()
+    verify_approval(capsys)
+
