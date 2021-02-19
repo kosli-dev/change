99b26ce576b6737723bacd505c4d12824cda6d65
Jon Jagger <jon@jaggersoft.com>
2021-02-19 05:48:27 +0000

    First test for LogArtifact() on bitbucket passes with shameless green

--- tests/unit/test_m_bitbucket_log_artifact.py
+++ tests/unit/test_m_bitbucket_log_artifact.py
@@ -6,14 +6,18 @@ from tests.utils import *
 APPROVAL_DIR = "tests/unit/approved_executions"
 APPROVAL_FILE = "test_m_bitbucket_log_artifact"
 
+DOMAIN = "app.compliancedb.com"
 API_TOKEN = "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4"
-#BUILD_URL = 'https://gitlab/build/1456'
 BUILD_NUMBER = '1975'
-
+COMMIT = "abc50c8a53f79974d615df335669b59fb56a4ed3"
+PROTOCOL = "docker://"
+IMAGE_NAME = "acme/road-runner:4.67"
+SHA256 = "aacdaef69c676c2466571d3288880d559ccc2032b258fc5e73f99a103db462ee"
+BB = "https://bitbucket.org"
+ORG = 'acme'
+REPO = 'road-runner'
 
 def test_required_env_vars(mocker): #capsys, mocker):
-    commit = "abc50c8a53f79974d615df335669b59fb56a4ed3"
-    image_name = "acme/road-runner:4.67"
 
     merkelypipe = "pipefile.json"
 
@@ -21,23 +25,22 @@ def test_required_env_vars(mocker): #capsys, mocker):
         "CDB_COMMAND": "put_artifact_image",
         "CDB_PIPELINE_DEFINITION": f"tests/data/{merkelypipe}",
         "CDB_API_TOKEN": API_TOKEN,
-        "CDB_ARTIFACT_DOCKER_IMAGE": image_name,
-        "BITBUCKET_COMMIT": commit,
+        "CDB_ARTIFACT_DOCKER_IMAGE": IMAGE_NAME,
+        "BITBUCKET_COMMIT": COMMIT,
         "BITBUCKET_BUILD_NUMBER": BUILD_NUMBER,
-        "BITBUCKET_WORKSPACE": "acme",
-        "BITBUCKET_REPO_SLUG": "road-runner"
+        "BITBUCKET_WORKSPACE": ORG,
+        "BITBUCKET_REPO_SLUG": REPO
     }
-    sha256 = "aacdaef69c676c2466571d3288880d559ccc2032b258fc5e73f99a103db462ee"
     set_env_vars = {
-        'CDB_ARTIFACT_GIT_URL': 'https://bitbucket.org/acme/road-runner/commits/' + commit,
-        'CDB_ARTIFACT_GIT_COMMIT': commit,
+        'CDB_ARTIFACT_GIT_URL': f"{BB}/{ORG}/{REPO}/commits/{COMMIT}",
+        'CDB_ARTIFACT_GIT_COMMIT': COMMIT,
         'CDB_BUILD_NUMBER': BUILD_NUMBER,
-        'CDB_CI_BUILD_URL': 'https://bitbucket.org/acme/road-runner/addon/pipelines/home#!/results/1975',
-        'CDB_ARTIFACT_SHA': sha256,
+        'CDB_CI_BUILD_URL': f'{BB}/{ORG}/{REPO}/addon/pipelines/home#!/results/{BUILD_NUMBER}',
+        'CDB_ARTIFACT_SHA': SHA256,
     }
     with dry_run(env, set_env_vars):
         pipe = BitbucketPipe(pipe_metadata='/pipe.yml', schema=schema)
-        mocker.patch('cdb.cdb_utils.calculate_sha_digest_for_docker_image', return_value=sha256)
+        mocker.patch('cdb.cdb_utils.calculate_sha_digest_for_docker_image', return_value=SHA256)
         pipe.run()
 
     #verify_approval(capsys)
@@ -51,15 +54,16 @@ def test_required_env_vars(mocker): #capsys, mocker):
     _old_blurb, old_method, old_payload, old_url = extract_blurb_method_payload_url(old_approval)
 
     expected_method = "Putting"
-    expected_url = "https://app.compliancedb.com/api/v1/projects/merkely/test-pipefile/artifacts/"
+    expected_url = f"https://{DOMAIN}/api/v1/projects/merkely/test-pipefile/artifacts/"
+
     expected_payload = {
-        "build_url": f"https://bitbucket.org/acme/road-runner/addon/pipelines/home#!/results/{BUILD_NUMBER}",
-        "commit_url": f"https://bitbucket.org/acme/road-runner/commits/{commit}",
+        "build_url": f"{BB}/{ORG}/{REPO}/addon/pipelines/home#!/results/{BUILD_NUMBER}",
+        "commit_url": f"{BB}/{ORG}/{REPO}/commits/{COMMIT}",
         "description": f"Created by build {BUILD_NUMBER}",
-        "filename": image_name,
-        "git_commit": commit,
+        "filename": IMAGE_NAME,
+        "git_commit": COMMIT,
         "is_compliant": False,
-        "sha256": sha256,
+        "sha256": SHA256,
     }
 
     # verify data from approved cdb text file
@@ -68,39 +72,31 @@ def test_required_env_vars(mocker): #capsys, mocker):
     assert old_url == expected_url
 
     # make merkely call
-    protocol = "docker://"
-    ev = new_log_artifact_env(commit)
-    ev["MERKELY_FINGERPRINT"] = f"{protocol}{image_name}"
-    #merkelypipe = "Merkelypipe.compliancedb.json"
+    ev = new_log_artifact_env()
     merkelypipe = "pipefile.json"
     with dry_run(ev) as env, scoped_merkelypipe_json(filename=merkelypipe):
-        with MockDockerFingerprinter(image_name, sha256) as fingerprinter:
+        with MockDockerFingerprinter(IMAGE_NAME, SHA256) as fingerprinter:
             method, url, payload = run(env, fingerprinter, None)
 
     # verify matching data
     assert method == expected_method
     assert url == expected_url
-    #assert payload == expected_payload WIP
+    assert payload == expected_payload
 
 
-def new_log_artifact_env(commit=None):
-    if commit is None:
-        commit = "abc50c8a53f79974d615df335669b59fb56a4ed3"
-    domain = "app.compliancedb.com"
+def new_log_artifact_env():
     return {
         "MERKELY_COMMAND": "log_artifact",
         "MERKELY_API_TOKEN": API_TOKEN,
-        "MERKELY_HOST": f"https://{domain}",
-        "MERKELY_FINGERPRINT": 'docker://acme/road-runner:2.3',
-        "MERKELY_CI_BUILD_URL": 'XXXXXX',
+        "MERKELY_HOST": f"https://{DOMAIN}",
+        "MERKELY_FINGERPRINT": f"{PROTOCOL}{IMAGE_NAME}",
+        "MERKELY_IS_COMPLIANT": "FALSE",
+
+        "MERKELY_CI_BUILD_URL": f"{BB}/{ORG}/{REPO}/addon/pipelines/home#!/results/{BUILD_NUMBER}",
         "MERKELY_CI_BUILD_NUMBER": BUILD_NUMBER,
-        "MERKELY_ARTIFACT_GIT_URL": commit_url(commit),
-        "MERKELY_ARTIFACT_GIT_COMMIT": commit,
-        "MERKELY_IS_COMPLIANT": "TRUE",
 
-        "BITBUCKET_COMMIT": commit,
+        "MERKELY_ARTIFACT_GIT_URL": f"{BB}/{ORG}/{REPO}/commits/{COMMIT}",
+        "MERKELY_ARTIFACT_GIT_COMMIT": COMMIT,
+
+        #"BITBUCKET_COMMIT": COMMIT,
     }
-
-
-def commit_url(commit):
-    return f"https://github/me/project/commit/{commit}"
