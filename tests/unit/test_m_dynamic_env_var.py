c3f59e78e865eb2d773751d222d66b2c74270b41
Jon Jagger <jon@jaggersoft.com>
2021-02-20 17:26:47 +0000

    Add test for non ambigous expansion of dynamic env-var

--- tests/unit/test_m_dynamic_env_var.py
+++ tests/unit/test_m_dynamic_env_var.py
@@ -3,6 +3,13 @@ from env_vars import CIBuildNumberEnvVar
 from pytest import raises
 
 
+def test_value_expands_when_non_abiguous_expansion():
+    gh = {"GITHUB_RUN_ID": "23"}
+    assert CIBuildNumberEnvVar(gh).value == "23"
+    bb = {"BITBUCKET_BUILD_NUMBER": "134"}
+    assert CIBuildNumberEnvVar(bb).value == "134"
+
+
 def test_value_raises_when_no_default_expansion():
     ev = CIBuildNumberEnvVar({})
     with raises(ChangeError) as exc:
