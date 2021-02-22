742ecdb9818b7983c6c5d477ab042e936f26103c
Jon Jagger <jon@jaggersoft.com>
2021-02-22 13:35:27 +0000

    Add cdb half of bitbucket test for log_evidence

--- tests/unit/test_m_bitbucket_log_evidence.py
+++ tests/unit/test_m_bitbucket_log_evidence.py
@@ -1,9 +1,10 @@
+from cdb.put_evidence import put_evidence
 from commands import run
 
 from tests.utils import *
 
 APPROVAL_DIR = "tests/unit/approved_executions"
-APPROVAL_FILE = "test_m_log_evidence"
+APPROVAL_FILE = "test_m_bitbucket_log_evidence"
 
 DOMAIN = "app.compliancedb.com"
 API_TOKEN = "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4"
@@ -22,9 +23,31 @@ DESCRIPTION = "branch coverage"
 EVIDENCE_TYPE = "unit_test"
 
 
-def test_bitbucket(capsys):
+def test_bitbucket(capsys, mocker):
     # The original bitbucket code did not do a translation for put_evidence
-    # TODO: call directly into source/cdb/put_evidence.py
+
+    env = {
+        "CDB_API_TOKEN": API_TOKEN,
+        "CDB_ARTIFACT_DOCKER_IMAGE": IMAGE_NAME,
+        "CDB_IS_COMPLIANT": "TRUE",
+        "CDB_EVIDENCE_TYPE": EVIDENCE_TYPE,
+        "CDB_DESCRIPTION": DESCRIPTION,
+        "CDB_CI_BUILD_URL": f"https://{BB}/{ORG}/{REPO}/addon/pipelines/home#!/results/{BUILD_NUMBER}",
+    }
+    set_env_vars = {}
+
+    with dry_run(env, set_env_vars):
+        mocker.patch('cdb.cdb_utils.calculate_sha_digest_for_docker_image', return_value=SHA256)
+        put_evidence("tests/data/Merkelypipe.acme-roadrunner.json")
+    verify_approval(capsys, ["out"])
+
+    # extract data from approved cdb text file
+    import inspect
+    this_test = inspect.stack()[0].function
+    approved = f"{APPROVAL_DIR}/{APPROVAL_FILE}.{this_test}.approved.txt"
+    with open(approved) as file:
+        old_approval = file.read()
+    _old_blurb, old_method, old_payload, old_url = extract_blurb_method_payload_url(old_approval)
 
     expected_method = "Putting"
     expected_url = f"https://{DOMAIN}/api/v1/projects/{ORG}/{REPO}/artifacts/{SHA256}"
@@ -36,6 +59,11 @@ def test_bitbucket(capsys):
         },
         "evidence_type": EVIDENCE_TYPE
     }
+
+    # verify data from approved cdb text file
+    assert old_method == expected_method
+    assert old_url == expected_url
+    assert old_payload == expected_payload
 
     # make merkely call
     ev = new_log_evidence_env()
