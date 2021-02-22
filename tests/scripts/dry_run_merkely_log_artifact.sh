72128bf9595352cc7c01af3b83e516cced127d73
Jon Jagger <jon@jaggersoft.com>
2021-02-22 11:11:13 +0000

    Update test script env-vars

--- tests/scripts/dry_run_merkely_log_artifact.sh
+++ tests/scripts/dry_run_merkely_log_artifact.sh
@@ -3,14 +3,14 @@
 export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 
 MERKELY_COMMAND=log_artifact
-MERKELY_DOCKER_IMAGE=python:3.7-alpine
+MERKELY_FINGERPRINT=docker://python:3.7-alpine
 MERKELY_IS_COMPLIANT=TRUE
 MERKELY_API_TOKEN=5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4
 
 BITBUCKET_WORKSPACE=acme
 BITBUCKET_REPO_SLUG=road-runner
 BITBUCKET_COMMIT=abc50c8a53f79974d615df335669b59fb56a4ed3
-BITBUCKET_BUILD_NUMBER=1975,
+BITBUCKET_BUILD_NUMBER=1975
 
 function merkely_log_artifact()
 {
@@ -21,7 +21,7 @@ function merkely_log_artifact()
         --env CDB_DRY_RUN=TRUE \
         --env MERKELY_COMMAND=log_artifact \
         \
-        --env MERKELY_FINGERPRINT="docker://${MERKELY_DOCKER_IMAGE}" \
+        --env MERKELY_FINGERPRINT=${MERKELY_FINGERPRINT} \
         --env MERKELY_IS_COMPLIANT=${MERKELY_IS_COMPLIANT} \
         --env BITBUCKET_WORKSPACE=${BITBUCKET_WORKSPACE} \
         --env BITBUCKET_REPO_SLUG=${BITBUCKET_REPO_SLUG} \
