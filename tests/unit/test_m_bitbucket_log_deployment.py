bccafb4727f4af2baea39de1eb31511f9e6a7e08
Jon Jagger <jon@jaggersoft.com>
2021-02-22 13:59:02 +0000

    Add cdb half of bitbucket test for log_deployment

--- tests/unit/test_m_bitbucket_log_deployment.py
+++ tests/unit/test_m_bitbucket_log_deployment.py
@@ -1,9 +1,10 @@
+from cdb.create_deployment import create_deployment
 from commands import run
 
 from tests.utils import *
 
 APPROVAL_DIR = "tests/unit/approved_executions"
-APPROVAL_FILE = "test_m_log_evidence"
+APPROVAL_FILE = "test_m_bitbucket_log_deployment"
 
 DOMAIN = "app.compliancedb.com"
 API_TOKEN = "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4"
@@ -18,15 +19,38 @@ PROTOCOL = "docker://"
 IMAGE_NAME = "acme/road-runner:4.67"
 SHA256 = "aacdaef69c676c2466571d3288880d559ccc2032b258fc5e73f99a103db462ee"
 
+DESCRIPTION = f"Deployment of {IMAGE_NAME}:aacdaef to docker hub"
+ENVIRONMENT = "dockerhub"
 USER_DATA = "/app/tests/data/user_data.json"
-DESCRIPTION = "branch coverage"
-ENVIRONMENT = "production"
-USER_DATA = "/app/tests/data/user_data.json"
+USER_DATA_JSON = {'status': 'deployed'}
 
 
-def test_bitbucket(capsys):
+def test_bitbucket(capsys, mocker):
     # The original bitbucket code did not do a BitBucket translation for create_deployment
-    # TODO: call directly into source/cdb/create_deployment.py
+    env = {
+        "CDB_HOST": "https://app.compliancedb.com",
+        "CDB_API_TOKEN": "5199831f4ee3b79e7c5b7e0ebe75d67aa66e79d4",
+        "CDB_ARTIFACT_DOCKER_IMAGE": "acme/runner:4.56",
+        "CDB_ENVIRONMENT": ENVIRONMENT,
+        "CDB_DESCRIPTION": DESCRIPTION,
+        "CDB_CI_BUILD_URL": f"https://{BB}/{ORG}/{REPO}/addon/pipelines/home#!/results/{BUILD_NUMBER}",
+        "CDB_USER_DATA": USER_DATA,
+    }
+    set_env_vars = {'CDB_ARTIFACT_SHA': SHA256}
+
+    with dry_run(env, set_env_vars):
+        mocker.patch('cdb.cdb_utils.calculate_sha_digest_for_docker_image', return_value=SHA256)
+        create_deployment("tests/data/Merkelypipe.acme-roadrunner.json")
+
+    verify_approval(capsys, ["out"])
+
+    # extract data from approved cdb text file
+    import inspect
+    this_test = inspect.stack()[0].function
+    approved = f"{APPROVAL_DIR}/{APPROVAL_FILE}.{this_test}.approved.txt"
+    with open(approved) as file:
+        old_approval = file.read()
+    _old_blurb, old_method, old_payload, old_url = extract_blurb_method_payload_url(old_approval)
 
     expected_method = "Posting"
     expected_url = f"https://{DOMAIN}/api/v1/projects/{ORG}/{REPO}/deployments/"
@@ -35,8 +59,13 @@ def test_bitbucket(capsys):
         "build_url": f"https://{BB}/{ORG}/{REPO}/addon/pipelines/home#!/results/{BUILD_NUMBER}",
         "description": DESCRIPTION,
         "environment": ENVIRONMENT,
-        "user_data": {'status': 'deployed'},
+        "user_data": USER_DATA_JSON,
     }
+
+    # verify data from approved cdb text file
+    assert old_method == expected_method
+    assert old_url == expected_url
+    assert old_payload == expected_payload
 
     # make merkely call
     ev = new_log_deployment_env()
