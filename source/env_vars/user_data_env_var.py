ed8ea19ad788e05c07959a174d44e37525aed493
Jon Jagger <jon@jaggersoft.com>
2021-02-24 13:31:10 +0000

    Docs update

--- source/env_vars/user_data_env_var.py
+++ source/env_vars/user_data_env_var.py
@@ -3,9 +3,9 @@ from env_vars import DefaultedEnvVar
 DEFAULT = {}
 
 NOTES = "\n".join([
-    "If provided, a filename whose json content to embed in the deployment.",
-    "The filename must be volume-mounted in the container.",
-    f"If not provided, defaults to {DEFAULT}",
+    "A pathed filename containing json content to embed in the data logged to Merkely.",
+    "The pathed filename must be volume-mounted in the container.",
+    f"If not provided, the json content defaults to {DEFAULT}",
 ])
 
 
