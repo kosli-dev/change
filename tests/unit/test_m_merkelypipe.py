b582e9b6d9a86c5a2300086e7b362db6fe9c00fa
Jon Jagger <jon@jaggersoft.com>
2021-03-10 10:34:11 +0000

    Add tests for MERKELYPIPE_PATH env-var

--- tests/unit/test_m_merkelypipe.py
+++ tests/unit/test_m_merkelypipe.py
@@ -4,6 +4,21 @@ from tests.utils import *
 from pytest import raises
 
 
+def test_defaults_to_Merkelypipe_dot_json_in_data_dir():
+    os_env = {}
+    external = External(env=os_env)
+    with ScopedFileCopier('/app/tests/data/Merkelypipe.json', '/data/Merkelypipe.json'):
+        json = external.merkelypipe
+    assert json['owner'] == 'merkely-test'
+
+
+def test_env_var_can_override_the_default():
+    os_env = {"MERKELYPIPE_PATH": "/app/tests/data/Merkelypipe.acme-roadrunner.json"}
+    external = External(env=os_env)
+    json = external.merkelypipe
+    assert json['owner'] == 'acme'
+
+
 def test_raises_when_not_found(capsys):
     with dry_run(core_env_vars()) as env, raises(ChangeError):
         # no /Merkelypipe.json
