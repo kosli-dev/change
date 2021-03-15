8a08f53656ff2aa9187283fd1b5760a94c437671
Jon Jagger <jon@jaggersoft.com>
2021-03-15 14:32:06 +0000

    Docs: use Command.names() instead of explicit list of command names

---
+++ tests/unit/test_m_command.py
@@ -0,0 +1,28 @@
+from commands import run, External
+from errors import ChangeError
+from tests.utils import *
+from pytest import raises
+
+
+def test_raises_when_merkely_command_not_set(capsys):
+    ev = core_env_vars()
+    ev.pop("MERKELY_COMMAND")
+
+    with dry_run(ev) as env, raises(ChangeError):
+            run(External(env=env))
+
+
+def test_raises_when_merkely_command_is_empty_string(capsys):
+    ev = core_env_vars()
+    ev["MERKELY_COMMAND"] = ""
+
+    with dry_run(ev) as env, raises(ChangeError):
+        run(External(env=env))
+
+
+def test_raises_when_merkely_command_is_unknown(capsys):
+    ev = core_env_vars()
+    ev["MERKELY_COMMAND"] = "wibble"
+
+    with dry_run(ev) as env, raises(ChangeError):
+        run(External(env=env))
