91af8cc6432c41585053c39e5eff559105d057b7
Jon Jagger <jon@jaggersoft.com>
2021-03-25 10:10:07 +0000

    Delete dead cdb/put_bitbucket_pr.py

--- source/cdb/put_bitbucket_pr.py
+++
@@ -1,6 +0,0 @@
-from cdb.bitbucket import put_bitbucket_pull_request
-from cdb.cdb_utils import parse_cmd_line
-
-if __name__ == '__main__':
-    project_config_file = parse_cmd_line()
-    put_bitbucket_pull_request(project_config_file)
