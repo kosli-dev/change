af80572b9e811896d95eeb473d9cdea113745fc0
Jon Jagger <jon@jaggersoft.com>
2021-03-25 09:12:28 +0000

    Delete dead code

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -3,7 +3,6 @@ from .scoped_dir_copier import *
 from .scoped_file_copier import *
 from .scoped_merkelypipe_json import *
 from .silent import *
-from .inject_env_vars import *
 from .retry_backoff_factor import *
 from .stub_http_503 import *
 from .verify_approval import *
