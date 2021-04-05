1c6f63e1f1a8bed23bd4a08ddab7565f345f95a2
Jon Jagger <jon@jaggersoft.com>
2021-04-05 19:06:53 +0100

    Use simpler format-strings in http-retry

--- source/lib/http_retry.py
+++ source/lib/http_retry.py
@@ -8,6 +8,22 @@ The logged message does _not_ say 'Press Control^C to exit.'
 because Control^C requires a runtime environment with a tty.
 That would require a [docker run -it] which would fail in the
 target environment, a CI pipeline, which invariably has no tty.
+
+Originally we implemented http retries with this Retry class:
+https://github.com/urllib3/urllib3/blob/master/src/urllib3/util/retry.py
+https://urllib3.readthedocs.io/en/latest/reference/urllib3.util.html#module-urllib3.util.retry
+We soon abandoned it.
+- The only way we could mock the deeply occurring http calls was with httpretty
+  which mocks at the socket level and has no way to mock only the
+  first N calls in a retry situation. Thus it was impossible to test
+  the happy-path scenario of a few 503's followed by a 200/201.
+- It proved impossible create a Retry subclass containing attributes because
+  of clever meta-programming in its __init__, new, and __getattr__
+- Its empirical behaviour differed from its limited documented behaviour.
+  Eg, its sleep backoff actually had _no_ sleep before the first retry.
+- Its implementation was hard to understand.
+  Eg, its crucial increment() method returned a _new_ Retry() object.
+- In short, it was complicated and hard to test.
 """
 
 from errors import ChangeError
@@ -27,23 +43,6 @@ class HttpRetryExhausted(ChangeError):
 
 
 class HttpRetry():
-    """
-    Originally we implemented http retries with this Retry class:
-    https://github.com/urllib3/urllib3/blob/master/src/urllib3/util/retry.py
-    https://urllib3.readthedocs.io/en/latest/reference/urllib3.util.html#module-urllib3.util.retry
-    We soon abandoned it.
-    - The only way we could mock the deeply occurring http calls was with httpretty
-      which mocks at the socket level and has no way to mock only the
-      first N calls in a retry situation. Thus it was impossible to test
-      the happy-path scenario of a few 503's followed by a 200/201.
-    - It proved impossible create a Retry subclass containing attributes because
-      of clever meta-programming in its __init__, new, and __getattr__
-    - Its empirical behaviour differed from its limited documented behaviour.
-      Eg, its sleep backoff actually had _no_ sleep before the first retry.
-    - Its implementation was hard to understand.
-      Eg, its crucial increment() method returned a _new_ Retry() object.
-    - In short, it was complicated and hard to test.
-    """
     def __init__(self):
         self._status_retry_list = [503]
         self._max_count = MAX_RETRY_COUNT
@@ -85,19 +84,19 @@ class HttpRetry():
     def _log(self, count, status, seconds):
         lhs = self._response_message(count, status)
         rhs = self._retry_message(count, status, seconds)
-        print("{}{}".format(lhs, rhs))
+        print(f"{lhs}{rhs}")
 
     def _response_message(self, count, status):
         if count == 0:
-            return "Response.status={}".format(status)
+            return f"Response.status={status}"
         else:
-            return "Retry {}/{}: response.status={}".format(count, self._max_count, status)
+            return f"Retry {count}/{self._max_count}: response.status={status}"
 
     def _retry_message(self, count, status, seconds):
         if not self._retry(status):
             return ""
         elif count < self._max_count:
-            return ", retrying in {} seconds...".format(seconds)
+            return f", retrying in {seconds} seconds..."
         else:
             return ""
 
