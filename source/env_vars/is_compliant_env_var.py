ec0045248e94f584fed12a83dfd2be1bc7357e0b
Jon Jagger <jon@jaggersoft.com>
2021-04-05 19:10:35 +0100

    Docs: tweak MERKELY_IS_COMPLIANT notes

--- source/env_vars/is_compliant_env_var.py
+++ source/env_vars/is_compliant_env_var.py
@@ -14,4 +14,4 @@ class IsCompliantEnvVar(RequiredEnvVar):
         return False, ""
 
     def doc_note(self, _ci_name, _command_name):
-        return "TRUE if the artifact is considered compliant from you build process."
+        return 'Set this to "TRUE" if the artifact is considered compliant from you build process.'
