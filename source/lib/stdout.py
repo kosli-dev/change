6834b3d2365f364cf31181850ae77d03ad72fec5
Jon Jagger <jon@jaggersoft.com>
2021-04-08 04:58:50 +0100

    Add comment

--- source/lib/stdout.py
+++ source/lib/stdout.py
@@ -6,4 +6,10 @@ class Stdout():
     def print(self, string):
         # Intention is write string to a io.StringIO
         # object and then drop capsys in all tests.
+        #
+        #    import io
+        #    stdout = io.StringIO()
+        #    stdout.write("Hello ")
+        #    stdout.write("world")
+        #    assert stdout.getvalue() == "Hello world"
         builtins.print(string)
