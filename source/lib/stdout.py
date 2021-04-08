5b0c92eded16e495bb677e20fce3ad712306019b
Jon Jagger <jon@jaggersoft.com>
2021-04-08 04:56:45 +0100

    Add comment

--- source/lib/stdout.py
+++ source/lib/stdout.py
@@ -4,4 +4,6 @@ import builtins
 class Stdout():
 
     def print(self, string):
+        # Intention is write string to a io.StringIO
+        # object and then drop capsys in all tests.
         builtins.print(string)
