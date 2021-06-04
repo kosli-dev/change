b0066e0b959b85f988d09e5ea6e1daab7f362a34
Simon Castagna <sd.castagna@gmail.com>
2021-06-04 14:50:06 +0200

    Create js to update top level ToC links

--- docs.merkely.com/source/_static/merkely-custom.js
+++ docs.merkely.com/source/_static/merkely-custom.js
@@ -3,8 +3,26 @@
 $(document).ready(function () {
    // Open a non-local href target in a new tab/window whilst avoiding
    // a wrestling match with non-existent Sphinx nodes documentation.
-   const hrefs = $("a[href^='http']");
-   hrefs.attr('target','_blank');
-   hrefs.attr('rel', 'noopener noreferrer'); // secure linking site
+   const $hrefs = $("a[href^='http']");
+   $hrefs.attr('target','_blank');
+   $hrefs.attr('rel', 'noopener noreferrer'); // secure linking site
 
+   // This fix is used because of the legacy situation, a better solution
+   // will have to be implemented
+   const $topLevelLinks = $('.toctree-l1>a');
+   $topLevelLinks.each(function() {
+      const $link = $(this);
+      if ($link.text().trim() === 'Quick Start'){
+         $link.attr('href', '../tutorial/pipefile.html')
+      }
+      if ($link.text().trim() === 'Concepts'){
+         $link.attr('href', '../concepts/devops_change_is_the_new_normal.html')
+      }
+      if ($link.text().trim() === 'Command Reference'){
+         $link.attr('href', '../reference/declare_pipeline.html')
+      }
+      if ($link.text().trim() === 'Fingerprint Reference'){
+         $link.attr('href', '../fingerprints/docker_fingerprint.html')
+      }
+   });
 });
