2b177a38878bb6dffd7e0d551d657564553ef95c
Jon Jagger <jon@jaggersoft.com>
2021-09-27 15:13:44 +0100

    Add simpler dir:// protocol test that is easy to replicate on server side in Go

--- tests/unit/test_dir_fingerprinter.py
+++ tests/unit/test_dir_fingerprinter.py
@@ -2,6 +2,7 @@ from errors import ChangeError
 from fingerprinters import DirFingerprinter
 from pytest import raises
 import os
+import shutil
 
 DIR_PROTOCOL = "dir://"
 
@@ -16,6 +17,18 @@ def test_empty_dir_properties(tmp_path):
     assert fingerprinter.artifact_name(string) == path
     assert fingerprinter.artifact_basename(string) == basename
     assert fingerprinter.sha(string) == '0e64e0a7586808ddf0b4e12487006de037b13a3baec4b80293f012b49b84fdeb'
+
+
+def test_dir_with_one_file_with_known_content():
+    fingerprinter = DirFingerprinter()
+    dir = f"tmp/test_dir_with_one_file_with_known_content"
+
+    os.mkdir(f"/{dir}")
+    with open(f"/{dir}/file.extra", "w+") as file:
+        file.write("this is known extra content")
+
+    assert fingerprinter.sha(f"{DIR_PROTOCOL}{dir}") == '9b9d0a0f475c2e94e21a10a5d05438783bd1effeda27d55ef4f766c8e2c715b2'
+    shutil.rmtree(f"/{dir}")
 
 
 def test_handles_only_its_own_protocol():
